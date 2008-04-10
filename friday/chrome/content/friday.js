492c75278b4d72a3309ff63fae0dc138dba5bf57
Atul Varma <varmaa@toolness.com>
2008-04-10 16:23:27 -0700

    'enter command:' overlay now displays properly

--- friday/chrome/content/friday.js
+++ friday/chrome/content/friday.js
@@ -3,6 +3,9 @@ friday = {
 openWindow: function()
 {
     dump("hai\n");
+    msgPanel = document.getElementById("transparent-msg-panel");
+    msgPanel.openPopupAtScreen(0, 0, false);
+    dump("done\n");
 }
 
 }
