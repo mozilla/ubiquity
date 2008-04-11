8d255173fb923ba1ca17d21fcb43e5c81c9a73e2
Atul Varma <varmaa@toolness.com>
2008-04-10 17:50:32 -0700

    Last-entered input is auto-selected when command mode is entered.

--- friday/chrome/content/friday.js
+++ friday/chrome/content/friday.js
@@ -2,12 +2,19 @@ friday = {
 
 needsToShow: false,
 
+onTextEntered: function()
+{
+    msgPanel = document.getElementById("transparent-msg-panel");
+    msgPanel.hidePopup();
+},
+
 onShown: function()
 {
     if (friday.needsToShow)
     {
         textBox = document.getElementById("cmd-entry");
         textBox.focus();
+        textBox.select();
         friday.needsToShow = false;
     }
 },
