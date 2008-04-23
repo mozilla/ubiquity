2bc386a07631a3985603c20ca561fa06c2207f69
Atul Varma <varmaa@toolness.com>
2008-04-23 16:18:37 -0700

    Added a setTimeout() function to the cmdutils that is similar to the eponymous HTML window method.

--- friday/chrome/content/cmdutils.js
+++ friday/chrome/content/cmdutils.js
@@ -1,3 +1,28 @@
+function __TimerCallback(callback)
+{
+    Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+    var Ci = Components.interfaces;
+
+    this._callback = callback;
+    this.QueryInterface = XPCOMUtils.generateQI([Ci.nsITimerCallback]);
+}
+
+__TimerCallback.prototype = {
+    notify : function(timer) {
+        this._callback();
+    }
+};
+
+function setTimeout(callback, delay)
+{
+    var classObj = Components.classes["@mozilla.org/timer;1"];
+    var timer = classObj.createInstance(Components.interfaces.nsITimer);
+
+    timer.initWithCallback(new __TimerCallback(callback),
+                           delay,
+                           classObj.TYPE_ONE_SHOT);
+}
+
 function url(spec) {
     var classObj = Components.classes["@mozilla.org/network/io-service;1"];
     var ios = classObj.getService(Components.interfaces.nsIIOService);
