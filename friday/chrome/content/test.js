46709492e540092d2f9e820c7d4a75a2b02be81c
Atul Varma <varmaa@toolness.com>
2008-04-23 10:06:27 -0700

    Reindentation of code, purely cosmetic.

--- friday/chrome/content/test.js
+++ friday/chrome/content/test.js
@@ -9,63 +9,61 @@ function TestCase(func)
 }
 
 TestCase.prototype = {
+    run : function()
+    {
+        this.__func();
+    },
 
-run : function()
-{
-    this.__func();
-},
+    assertIsDefined : function(condition, msg)
+    {
+        if (condition == undefined)
+            throw new AssertionError(msg);
+    },
 
-assertIsDefined : function(condition, msg)
-{
-    if (condition == undefined)
-        throw new AssertionError(msg);
-},
-
-assert : function(condition, msg)
-{
-    if (!condition)
-        throw new AssertionError(msg);
-}
-
+    assert : function(condition, msg)
+    {
+        if (!condition)
+            throw new AssertionError(msg);
+    }
 };
 
-TestSuite = {
-start : function()
-{
-    var output = window.document.getElementById( "test-output" );
-    var parent = window;
-
-    var tests = [];
-
-    for (prop in parent)
-        if (prop.indexOf("test") == 0)
-            tests.push(parent[prop]);
-
-    var successes = 0;
-    var failures = 0;
-
-    for each (test in tests)
+var TestSuite = {
+    start : function()
     {
-        var testCase = new TestCase(test);
-        try {
-            dump("Running test: "+test.name+"\n");
-            testCase.run();
-            successes += 1;
-        } catch (e) {
-            var html = ("<p class=\"error\">Error in test " +
-                        test.name + ": " + e.message);
-            if (e.fileName)
-                html += (" (in " + e.fileName +
-                         ", line " + e.lineNumber + ")");
-            html += "</p>";
-            output.innerHTML += html;
-            failures += 1;
-        }
-    }
-    var total = successes + failures;
+        var output = window.document.getElementById( "test-output" );
+        var parent = window;
 
-    output.innerHTML += ("<p>" + successes + " out of " +
-                         total + " tests successful (" + failures +
-                         " failed).</p>");
-}
+        var tests = [];
+
+        for (prop in parent)
+            if (prop.indexOf("test") == 0)
+                tests.push(parent[prop]);
+
+        var successes = 0;
+        var failures = 0;
+
+        for each (test in tests)
+        {
+            var testCase = new TestCase(test);
+            try {
+                dump("Running test: "+test.name+"\n");
+                testCase.run();
+                successes += 1;
+            } catch (e) {
+                var html = ("<p class=\"error\">Error in test " +
+                            test.name + ": " + e.message);
+                if (e.fileName)
+                    html += (" (in " + e.fileName +
+                             ", line " + e.lineNumber + ")");
+                html += "</p>";
+                output.innerHTML += html;
+                failures += 1;
+            }
+        }
+        var total = successes + failures;
+
+        output.innerHTML += ("<p>" + successes + " out of " +
+                             total + " tests successful (" + failures +
+                             " failed).</p>");
+    }
 };
