06f0334681200e271af7c53ab9e4fb4b10743608
Atul Varma <varmaa@toolness.com>
2008-07-10 11:10:18 -0700

    harness.xul now automatically disables the XUL cache so that changes to it work.

--- ubiquity/chrome/content/harness.js
+++ ubiquity/chrome/content/harness.js
@@ -5,4 +5,14 @@ function onKeydown(aEvent) {
   }
 }
 
+function harnessSetup() {
+  // Disable the XUL cache.
+  Application.prefs.setValue("nglayout.debug.disable_xul_cache", true);
+}
+
+function harnessTeardown() {
+}
+
 window.addEventListener("keydown", onKeydown, false);
+window.addEventListener("load", harnessSetup, false);
+window.addEventListener("unload", harnessTeardown, false);
