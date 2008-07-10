df641698a3b7967053b3957ce2028989b77548bc
Atul Varma <varmaa@toolness.com>
2008-07-09 18:40:10 -0700

    Fixed a bug in harness.js.

--- ubiquity/chrome/content/harness.js
+++ ubiquity/chrome/content/harness.js
@@ -1,11 +1,6 @@
-function openCommandWindow() {
-  var panel = document.getElementById("transparent-msg-panel");
-  panel.openPopup(null, "", 0, 0, false, true);
-}
-
 function onKeydown(aEvent) {
   if (aEvent.keyCode == 88 && aEvent.ctrlKey) {
-    openCommandWindow();
+    gUbiquity.openWindow();
     aEvent.preventDefault();
   }
 }
