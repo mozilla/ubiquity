ed12decc1981aa6149760bf4bc424dfab5645145
Atul Varma <varmaa@toolness.com>
2008-07-17 17:03:31 -0700

    Made the iframe sandbox factory work much more nicely.

--- ubiquity/chrome/content/iframe_sandboxfactory.js
+++ ubiquity/chrome/content/iframe_sandboxfactory.js
@@ -1,22 +1,3 @@
-// Create some iframes to use as sandboxes; the window's onload()
-// won't be called until they're all loaded.  This is so the
-// IframeSandboxFactory can create sandboxes without requiring a
-// callback.
-
-var __iframes = [];
-var __NUM_IFRAMES = 10;
-var __currIframe = 0;
-
-for (var i = 0; i < __NUM_IFRAMES; i++) {
-  let iframe = window.document.createElementNS(
-    "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul",
-    "iframe"
-  );
-
-  window.document.documentElement.appendChild(iframe);
-  __iframes.push(iframe);
-}
-
 function IframeSandboxFactory(globals) {
   if (globals == undefined)
     globals = {};
@@ -25,10 +6,25 @@ function IframeSandboxFactory(globals) {
 
 IframeSandboxFactory.prototype = {
   makeSandbox: function(cb) {
-    if (__currIframe == __NUM_IFRAMES)
-      throw new Error("Maximum number of sandboxes reached.");
-    var iframe = __iframes[__currIframe];
-    __currIframe += 1;
+    let iframe = window.document.createElementNS(
+      "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul",
+      "iframe"
+    );
+
+    var finishedLoading = false;
+
+    iframe.onload = function() {
+      finishedLoading = true;
+    };
+
+    window.document.documentElement.appendChild(iframe);
+
+    var thread = Components.classes["@mozilla.org/thread-manager;1"]
+                 .getService(Components.interfaces.nsIThreadManager)
+                 .currentThread;
+
+    while (!finishedLoading)
+      thread.processNextEvent(true);
 
     var sandbox = iframe.contentWindow.wrappedJSObject;
 
