de268d06f03d44733fb9d6a14236a22514a0b461
jonathandicarlo@jonathan-dicarlos-macbook-pro.local <none@none>
2008-07-24 17:13:51 -0700

    Added selutils.js back in

---
+++ ubiquity/chrome/content/selutils.js
@@ -0,0 +1,43 @@
+
+
+function paramsToString(params) {
+  var string = "?";
+
+  for (key in params) {
+    string += escape(key) + "=" + escape(params[key]) + "&";
+  }
+
+  // Remove the trailing &
+  return string.substr(0, string.length - 1);
+}
+
+function getTextSelection(context) {
+  var focused = context.focusedElement;
+  var retval = "";
+
+  if (focused)
+    if (focused.selectionStart != focused.selectionEnd)
+      retval = focused.value.substring(focused.selectionStart,
+                                       focused.selectionEnd);
+  if (!retval) {
+    var sel = context.focusedWindow.getSelection();
+    if (sel.rangeCount >= 1)
+      retval = sel.toString();
+  }
+  return retval;
+}
+
+function ajaxGet(url, callbackFunction) {
+  var request = new window.XMLHttpRequest();
+  request.open("GET", url, true);
+  request.setRequestHeader("Content-Type",
+                           "application/x-www-form-urlencoded");
+
+  request.onreadystatechange = safeWrapper( function() {
+    if (request.readyState == 4 && request.status == 200)
+      if (request.responseText)
+        callbackFunction(request.responseText);
+  });
+
+  request.send(null);
+}
