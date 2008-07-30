c76e6aadd0c5cc847ba6f427ea83aef1d0a6f8f9
Abimanyu Raja <abimanyuraja@gmail.com>
2008-07-30 09:28:51 +0800

    Ported the close related tabs command

--- ubiquity/chrome/content/builtincmds.js
+++ ubiquity/chrome/content/builtincmds.js
@@ -654,3 +654,51 @@ CreateCommand({
     }); 
   }
 })
+
+
+
+// -----------------------------------------------------------------
+// TAB COMMANDS
+// -----------------------------------------------------------------
+
+
+CreateCommand({
+  name: "close.related.tabs",
+  takes: {"related word": arbText},
+  preview: function( pblock, query ) {
+    var relatedWord = query.toLowerCase();
+    if(relatedWord.length != 0){
+      var html = "Closes the following tabs that are related to <b style=\"color:yellow\">\"" + relatedWord + "\"</b> : <ul>";
+      var numTabs = 0;
+
+      Application.activeWindow.tabs.forEach(function(tab){
+        if ( tab.uri.spec.toLowerCase().match(relatedWord) || tab.document.title.toLowerCase().match(relatedWord)){
+      	  html += "<li>" + tab.document.title + "</li>";
+      	  numTabs++;
+        }
+      });
+
+      if(numTabs == 0){
+        html = "No tabs related to <b style=\"color:yellow\">\"" + relatedWord + "\"</b>";
+      }else{
+        html += "</ul>";
+      }
+    }else{
+      html = "Closes tabs related to the word";
+    }
+    jQuery(pblock).html( html );
+  },
+  execute: function( query ) {
+    var relatedWord = query.toLowerCase();
+    var numTabs = 0;
+
+    Application.activeWindow.tabs.forEach(function(tab){
+      if ( tab.uri.spec.toLowerCase().match(relatedWord) || tab.document.title.toLowerCase().match(relatedWord)){
+        tab.close();
+        numTabs++;
+      }
+    });
+    
+    displayMessage(numTabs + " tabs closed");
+  }
+})
