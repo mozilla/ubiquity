1f84badee903e0e08868398ec77fd6fbfc0eaf10
Maria Emerson <memerson@mozilla.com>
2008-08-08 17:28:30 -0700

    added edit-page command

--- ubiquity/chrome/content/builtincmds.js
+++ ubiquity/chrome/content/builtincmds.js
@@ -1051,7 +1051,7 @@ cmd_edit_page.preview = function( pblock ) {
 function cmd_save() {
   // TODO: works w/o wrappedJSObject in CmdUtils.getDocumentInsecure() call- fix this
   CmdUtils.getDocumentInsecure().body.contentEditable = 'false';
-  CmdUtils.getDocumentInsecure().designMode='off';
+  CmdUtils.getDocumentInsecure().designMode = 'off';
 
   var annotationService = Components.classes["@mozilla.org/browser/annotation-service;1"]
                           .getService(Components.interfaces.nsIAnnotationService);
@@ -1061,7 +1061,80 @@ function cmd_save() {
   var body = jQuery( CmdUtils.getDocumentInsecure().body );
 
   annotationService.setPageAnnotation(ioservice.newURI(window.content.location.href, null, null), "ubiquity/edit", body.html(), 0, 4);
+  
 }
+cmd_save.preview = function( pblock ) {
+  pblock.innerHTML = "Saves page edits. Undo with 'remove-annotations'";
+}
+
+pageLoad_restorePageAnnotations = function () {
+  var annotationService = Components.classes["@mozilla.org/browser/annotation-service;1"]
+                          .getService(Components.interfaces.nsIAnnotationService);
+  var ioservice = Components.classes["@mozilla.org/network/io-service;1"]
+                  .getService(Components.interfaces.nsIIOService);
+
+  var uri = ioservice.newURI(window.content.location.href, null, null);
+
+  var annotationNames = annotationService.getPageAnnotationNames(uri, {}); 
+
+  for (var i=0; i<annotationNames.length; i++) {
+
+    var annotationName, annotationValue;
+    var startNode, endNode;
+    var startXpath, endXpath;
+    var startOffset, endOffset;
+
+    if (annotationNames[i].match("ubiquity/delete/")) {
+      annotationName = annotationNames[i].substring(16);
+      annotationValue = annotationService.getPageAnnotation(uri, annotationNames[i]);
+
+      // get xpaths out of name
+      startXpath = annotationName.substring(0, annotationName.indexOf("#"));
+      endXpath = annotationName.substring(annotationName.indexOf("#") + 1);
+
+      // get offsets out of value
+      startOffset = parseInt(annotationValue.substring(0, annotationValue.indexOf("#")));
+      endOffset = parseInt(annotationValue.substring(annotationValue.indexOf("#") + 1));
+
+
+      // find the nodes from the xpaths
+      var iterator;
+      iterator = doc.evaluate(startXpath, doc, null, XPathResult.ANY_TYPE, null);
+      startNode = iterator.iterateNext();
+      iterator = doc.evaluate(endXpath, doc, null, XPathResult.ANY_TYPE, null);
+      endNode = iterator.iterateNext();
+
+
+      // delete the text content in between the start and end nodes
+      if (startNode == endNode) {
+        startNode.textContent = startNode.textContent.substring(0, startOffset) +
+          startNode.textContent.substring(endOffset);
+      }
+      else {
+        startNode.textContent = startNode.textContent.substring(0, startOffset);
+        var curNode = startNode.nextSibling;
+        while (curNode && (curNode != endNode)) {
+          curNode.textContent = "";
+          curNode = curNode.nextSibling;
+        }
+        endNode.textContent = endNode.textContent.substring(endOffset);
+      }
+
+    }
+
+    if (annotationNames[i] == "ubiquity/edit") {
+      // TODO: works w/o wrappedJSObject in CmdUtils.getDocumentInsecure() call- fix this
+      var body = jQuery( CmdUtils.getDocumentInsecure().body );
+
+      annotationValue = annotationService.getPageAnnotation(uri, annotationNames[i]);
+      body.html(annotationValue);
+
+      // TODO: Fix "TypeError: head is not defined" on some pages
+
+    }
+  }
+}
+
 cmd_save.preview = function( pblock ) {
   pblock.innerHTML = "Saves edits you've made to this page in an annotation.";
 }
@@ -1171,8 +1244,4 @@ function cmd_perm_delete() {
 cmd_perm_delete.preview = function( pblock ) {
   pblock.innerHTML = "Attempts to permanently delete the selected part of the"
     + " page. (Experimental!)";
-}
-
-function cmd_foo() {
-  displayMessage("Foo!");
 }
