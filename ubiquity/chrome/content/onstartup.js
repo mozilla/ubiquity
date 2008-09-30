0d2dc8fde879c9836459a48ae215b48a2620fbf0
Atul Varma <varmaa@toolness.com>
2008-09-30 16:42:27 -0500

    Had to move startup_openUbiquityWelcomePage() over to builtincmds.js because cmd_help() is defined in it and each script runs in its own sandbox now.

--- ubiquity/chrome/content/onstartup.js
+++ ubiquity/chrome/content/onstartup.js
@@ -2,17 +2,3 @@ function startup_setBasicPreferences() {
   // Allow JS chrome errors to show up in the error console.
   Application.prefs.setValue("javascript.options.showInConsole", true);
 }
-
-function startup_openUbiquityWelcomePage()
-{
-  const VERSION_PREF ="extensions.ubiquity.lastversion";
-
-  // Compare the version in our preferences from our version in the
-  // install.rdf.
-  var ext = Application.extensions.get("ubiquity@labs.mozilla.com");
-  var currVersion = Application.prefs.getValue(VERSION_PREF, "firstrun");
-  if (currVersion != ext.version) {
-    Application.prefs.setValue(VERSION_PREF, ext.version);
-    cmd_help();
-  }
-}
