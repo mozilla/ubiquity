87ca5dc17a8cc09a45950e9964c61d2f6e40f99a
Fernando Takai <fernando.takai@gmail.com>
2008-10-30 00:53:51 -0200

    added an email noun that can validate rfc822 compilant emails

--- ubiquity/chrome/content/nlparser/en/nountypes.js
+++ ubiquity/chrome/content/nlparser/en/nountypes.js
@@ -91,6 +91,29 @@ var noun_type_contact = {
   }
 };
 
+/*
+ * Noun that matches only emails based on the regexp
+ * found on http://iamcal.com/publish/articles/php/parsing_email by Cal Henderson
+ * This regexp is RFC822 compilant.
+ */
+var noun_type_email = {
+  _name: "email",
+  _regexp: new RegExp('^([^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+' +
+                      '|\\x22([^\\x0d\\x22\\x5c\\x80-\\xff]|\\x5c[\\x00-\\x7f])*\\x22)(\\x2e([^\\x00-\\x20\\x22' +
+                      '\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+|\\x22([^\\x0d\\x22\\x5c' +
+                      '\\x80-\\xff]|\\x5c\\x00-\\x7f)*\\x22))*\\x40([^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-' +
+                      '\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+|\\x5b([^\\x0d\\x5b-\\x5d\\x80-\\xff]|\\x5c[\\x00-' +
+                      '\\x7f])*\\x5d)(\\x2e([^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\' +
+                      'x5d\\x7f-\\xff]+|\\x5b([^\\x0d\\x5b-\\x5d\\x80-\\xff]|\\x5c[\\x00-\\x7f])*\\x5d))*$'),
+  suggest: function(text, html){
+    if(this._regexp.test(text)){
+      return [ CmdUtils.makeSugg(text) ];
+    }
+    
+    return [];
+  }
+}
+
 var noun_arb_text = {
   _name: "text",
   rankLast: true,
