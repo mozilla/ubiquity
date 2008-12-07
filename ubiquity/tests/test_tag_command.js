79ebfbe86276268efba634c7785808c580be551f
Atul Varma <varmaa@toolness.com>
2008-12-07 14:51:47 -0800

    Fixed a failing unit test that can't currently be run via xpcshell.

--- ubiquity/tests/test_tag_command.js
+++ ubiquity/tests/test_tag_command.js
@@ -1,40 +1,55 @@
 Components.utils.import("resource://ubiquity-tests/framework.js");
-Components.utils.import("resource://ubiquity-modules/setup.js");
-Components.utils.import("resource://ubiquity-modules/cmdmanager.js");
-Components.utils.import("resource://ubiquity-modules/cmdsource.js");
-Components.utils.import("resource://ubiquity-modules/parser/parser.js");
-
-function LOG(aMsg) {
-  aMsg = ("*** UBI TESTS: " + aMsg);
-  Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService).
-    logStringMessage(aMsg);
-}
-
-var services = UbiquitySetup.createServices();
-var cmdSource = services.commandSource;
-var nlParser = NLParser.makeParserForLanguage(UbiquitySetup.languageCode, [], []);
-var cmdManager = new CommandManager(cmdSource, services.messageService,
-                                    nlParser);
 
 const Ci = Components.interfaces;
 const Cc = Components.classes;
 
-var tagsvc = Cc["@mozilla.org/browser/tagging-service;1"].getService(Ci.nsITaggingService);
-var bmsvc = Cc["@mozilla.org/browser/nav-bookmarks-service;1"].getService(Ci.nsINavBookmarksService);
-var iosvc = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
-
-function uri(spec) {
-  return iosvc.newURI(spec, null, null);
-}
-
-function uriHasTags(aURI, aTags) {
-  var tags = tagsvc.getTagsForURI(aURI, {});
-  return aTags.every(function(aTag) {
-    return tags.indexOf(aTag) > -1;
-  }, this);
-}
+var module = this;
 
 function testTagCommand() {
+  try {
+    var bmsvc = Cc["@mozilla.org/browser/nav-bookmarks-service;1"]
+                .getService(Ci.nsINavBookmarksService);
+  } catch (e) {
+    // We're apparently running in xpcshell, which this test can't be
+    // performed with.
+    throw new this.SkipTestError();
+  }
+
+  var tagsvc = Cc["@mozilla.org/browser/tagging-service;1"]
+               .getService(Ci.nsITaggingService);
+  var iosvc = Cc["@mozilla.org/network/io-service;1"]
+              .getService(Ci.nsIIOService);
+
+  Components.utils.import("resource://ubiquity-modules/setup.js");
+  Components.utils.import("resource://ubiquity-modules/cmdmanager.js");
+  Components.utils.import("resource://ubiquity-modules/cmdsource.js");
+  Components.utils.import("resource://ubiquity-modules/parser/parser.js");
+
+  function LOG(aMsg) {
+    aMsg = ("*** UBI TESTS: " + aMsg);
+    Components.classes["@mozilla.org/consoleservice;1"]
+              .getService(Components.interfaces.nsIConsoleService)
+              .logStringMessage(aMsg);
+  }
+
+  var services = UbiquitySetup.createServices();
+  var cmdSource = services.commandSource;
+  var nlParser = NLParser.makeParserForLanguage(UbiquitySetup.languageCode,
+                                                [], []);
+  var cmdManager = new CommandManager(cmdSource, services.messageService,
+                                      nlParser);
+
+  function uri(spec) {
+    return iosvc.newURI(spec, null, null);
+  }
+
+  function uriHasTags(aURI, aTags) {
+    var tags = tagsvc.getTagsForURI(aURI, {});
+    return aTags.every(function(aTag) {
+      return tags.indexOf(aTag) > -1;
+    }, module);
+  }
+
   var testURI = uri("chrome://ubiquity/content/test.html");
 
   // for cleanup
@@ -54,17 +69,18 @@ function testTagCommand() {
   // test tag appended to existing tags
   cmdManager.updateInput("tag bar", context);
   cmdManager.execute(context);
-  this.assert(uriHasTags(testURI, ["foo", "bar"])); 
+  this.assert(uriHasTags(testURI, ["foo", "bar"]));
 
   // test add tags separated by spaces
   cmdManager.updateInput("tag baz bot", context);
   cmdManager.execute(context);
-  this.assert(uriHasTags(testURI, ["foo", "bar", "baz", "bot"])); 
+  this.assert(uriHasTags(testURI, ["foo", "bar", "baz", "bot"]));
 
   // test add tags separated by commas
   cmdManager.updateInput("tag bom, la bamba", context);
   cmdManager.execute(context);
-  this.assert(uriHasTags(testURI, ["foo", "bar", "baz", "bot", "bom", "la bamba"])); 
+  this.assert(uriHasTags(testURI, ["foo", "bar", "baz", "bot", "bom",
+                                   "la bamba"]));
 
   // cleanup
   tagsvc.untagURI(testURI, null);
