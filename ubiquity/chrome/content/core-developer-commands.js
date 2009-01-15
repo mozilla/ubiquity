aeaa8275780a8c0e680ae0e7ae271bbf28d6113f
Atul Varma <varmaa@toolness.com>
2009-01-15 12:14:32 -0500

    Added an early version of a core developer command feed.

---
+++ ubiquity/chrome/content/core-developer-commands.js
@@ -0,0 +1,30 @@
+// == Core Developer Commands ==
+//
+// This is the command feed for core developer commands.
+
+// === {{{markupTickets()}}} ===
+//
+// This function finds any text in the given JQuery query that looks like
+// a Trac ticket and hyperlinks the text to the ticket.
+function markupTickets(query) {
+  var html = query.html();
+  var regexp = /#([0-9]+)/g;
+  var template = '<a href="http://labs.toolness.com/trac/ticket/$1">#$1</a>';
+  query.html(html.replace(regexp, template));
+}
+
+// === {{{pageLoad_developerCommands()}}} ===
+//
+// This function is a page-load function that applies transformations
+// to certain developer-related web pages to provide added functionality.
+function pageLoad_developerCommands(doc) {
+  if (doc.location.protocol == "http:" &&
+      doc.location.host == 'hg.toolness.com') {
+    var regexp = /\/ubiquity-firefox\/rev\/([0-9a-f]+)/;
+    var match = doc.location.pathname.match(regexp);
+    if (match) {
+      var rev = match[1];
+      markupTickets(jQuery(".description", doc));
+    }
+  }
+}
