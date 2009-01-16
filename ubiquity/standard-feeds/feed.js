1a34ec1763b7a86923408209777d67ceaf5e87b6
Atul Varma <varmaa@toolness.com>
2009-01-16 11:20:52 -0500

    Added auto-laoder for command feed HTML pages, which allows them to be loaded on the local filesystem in Windows and also means it's easier to obey DRY in the HTML.

---
+++ ubiquity/standard-feeds/feed.js
@@ -0,0 +1,49 @@
+var Feed = {
+  SCRIPT_PREFIX: "scripts/",
+
+  SCRIPTS_TO_LOAD: ["docs/docs.css",
+                    "jquery.js", "wikicreole.js", "docs/docs.js"],
+
+  loadNextScript: function() {
+    if (this.SCRIPTS_TO_LOAD.length) {
+      var baseName = this.SCRIPTS_TO_LOAD.pop();
+      var e;
+      if (baseName.indexOf('.js') != -1) {
+        e = document.createElement("script");
+        e.src = this.SCRIPT_PREFIX + baseName;
+        e.type="text/javascript";
+        e.addEventListener("load",
+                           function() { Feed.loadNextScript(); },
+                           false);
+        document.body.appendChild(e);
+      } else {
+        e = document.createElement("link");
+        e.rel = "stylesheet";
+        e.type = "text/css";
+        e.href = this.SCRIPT_PREFIX + baseName;
+        document.body.appendChild(e);
+        this.loadNextScript();
+      }
+    } else {
+      // Trigger the onload/ready events of any scripts we just
+      // loaded.
+      var event = document.createEvent("HTMLEvents");
+      event.initEvent("load", true, true);
+      window.dispatchEvent(event);
+    }
+  }
+};
+
+window.addEventListener(
+  "load",
+  function initial() {
+    window.removeEventListener("load", initial, false);
+    Feed.SCRIPTS_TO_LOAD.reverse();
+    if (window.location.protocol == "file:")
+      // If we're on Windows, we don't have symlinks, so use the
+      // real path.
+      Feed.SCRIPT_PREFIX = "../" + Feed.SCRIPT_PREFIX;
+    Feed.loadNextScript();
+  },
+  false
+  );
