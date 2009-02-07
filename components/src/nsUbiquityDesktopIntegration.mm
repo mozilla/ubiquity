43acac32a8d23d1c1af9d55a71e8d784e5618bde
Atul Varma <varmaa@toolness.com>
2009-02-07 14:21:12 -0800

    Added nsIUbiquityDesktopIntegration.unregisterGlobalHotkey(), and more error-checking code.

--- components/src/nsUbiquityDesktopIntegration.mm
+++ components/src/nsUbiquityDesktopIntegration.mm
@@ -10,6 +10,12 @@ NS_IMPL_ISUPPORTS1(nsUbiquityDesktopIntegration,
                    nsIUbiquityDesktopIntegration)
 
 static nsISupports *gSingleton;
+static EventHotKeyRef gMyHotKeyRef;
+static EventHotKeyID gMyHotKeyID;
+static EventHandlerRef gEventHandlerRef;
+
+// TODO: This class should really be a singleton that's accessed
+// through the service manager.
 
 nsUbiquityDesktopIntegration::nsUbiquityDesktopIntegration()
 {
@@ -21,6 +27,11 @@ nsUbiquityDesktopIntegration::~nsUbiquityDesktopIntegration()
   /* destructor code */
 }
 
+// This is called by the operating system in what I believe is the 
+// main thread of Firefox, which I believe is also the UI thread.
+// Hopefully it's ok to use XPCOM here--in any case, Firefox doesn't
+// crash when we use it, so we're hopefully doing everything right!
+// - A.V.
 static OSStatus HotKeyHandler(EventHandlerCallRef nextHandler,
                               EventRef theEvent,
                               void *userData) {
@@ -46,21 +57,50 @@ NS_IMETHODIMP nsUbiquityDesktopIntegration::RegisterGlobalHotkey(
   PRInt32 modifiers
   )
 {
-  gSingleton = this;
-  this->AddRef();
+  if (gSingleton)
+    return NS_ERROR_FAILURE;
 
-  EventHotKeyRef gMyHotKeyRef;
-  EventHotKeyID gMyHotKeyID;
+  OSStatus result;
+
   EventTypeSpec eventType;
   eventType.eventClass=kEventClassKeyboard;
   eventType.eventKind=kEventHotKeyPressed;
-  InstallApplicationEventHandler(&HotKeyHandler,1,&eventType,NULL,NULL);
+  result = InstallEventHandler(GetApplicationEventTarget(),
+                               &HotKeyHandler,
+                               1,
+                               &eventType,
+                               NULL,
+                               &gEventHandlerRef);
+  if (result != noErr)
+    return NS_ERROR_FAILURE;
 
   gMyHotKeyID.signature='htk1';
   gMyHotKeyID.id=1;
-  RegisterEventHotKey(keyCode, modifiers, gMyHotKeyID, 
-                      GetApplicationEventTarget(), 0, &gMyHotKeyRef);
+  result = RegisterEventHotKey(keyCode, modifiers, gMyHotKeyID, 
+                               GetApplicationEventTarget(), 0,
+                               &gMyHotKeyRef);
+  if (result != noErr) {
+    RemoveEventHandler(gEventHandlerRef);
+    return NS_ERROR_FAILURE;
+  }
 
+  this->AddRef();
+  gSingleton = this;
+
+  return NS_OK;
+}
+
+NS_IMETHODIMP nsUbiquityDesktopIntegration::UnregisterGlobalHotkey()
+{
+  if (!gSingleton)
+    return NS_ERROR_FAILURE;
+
+  gSingleton = NULL;
+  this->Release();
+  if (UnregisterEventHotKey(gMyHotKeyRef) != noErr)
+    return NS_ERROR_FAILURE;
+  if (RemoveEventHandler(gEventHandlerRef) != noErr)
+    return NS_ERROR_FAILURE;
   return NS_OK;
 }
 
