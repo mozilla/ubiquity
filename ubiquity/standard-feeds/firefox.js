552dca9e167abdcd440380f22d57b9b756cbb358
Christian Sonne <cers@geeksbynature.dk>
2009-02-13 01:01:02 +0100

    Overhaul of the default feed firefox.js - a few bugfixes + a lot of restructuring

--- ubiquity/standard-feeds/firefox.js
+++ ubiquity/standard-feeds/firefox.js
@@ -50,9 +50,8 @@ var extApplication = { // helper method for correct quitting/restarting
 
 // exit firefox entirely
 CmdUtils.CreateCommand({
-  name: "exit",
+  name: "exit-firefox",
   description: "Exits firefox",
-  preview: function( pblock ) {pblock.innerHTML=this.description;},
   execute: function() {
     extApplication.quit();
   }
@@ -60,9 +59,8 @@ CmdUtils.CreateCommand({
 
 // restarts firefox
 CmdUtils.CreateCommand({
-  name: "restart",
+  name: "restart-firefox",
   description: "Restarts firefox",
-  preview: function( pblock ) {pblock.innerHTML=this.description;},
   execute: function() {
     extApplication.restart();
   }
@@ -71,9 +69,8 @@ CmdUtils.CreateCommand({
 // closes current firefox window
 // TODO: if last window is closed, we should offer to save session
 CmdUtils.CreateCommand({
-  name: "close",
+  name: "close-window",
   description: "Close current window",
-  preview: function( pblock ) {pblock.innerHTML=this.description},
   execute: function() {
     extApplication.close();
   }
@@ -83,7 +80,6 @@ CmdUtils.CreateCommand({
 CmdUtils.CreateCommand({
   name: "fullscreen",
   description: "Toggles fullscreen mode",
-  preview: function( pblock ) {pblock.innerHTML=this.description},
   execute: function() {
     var win = CmdUtils.getWindow();
     win.fullScreen = win.fullScreen ? false : true;
@@ -99,40 +95,38 @@ CmdUtils.CreateCommand({
   takes: {"tab name": noun_type_tab},
   icon: "chrome://ubiquity/skin/icons/tab_go.png",
   description: "Switches to the tab that matches the given name.",
-
   execute: function( tab ) {
     var tabName = tab.text;
     var tab = Utils.tabs.get(tabName);
     if (tab) {
       // TODO: window.focus() is missing on 1.9.2pre
-      if (tab._window && tab._window.focus)
+      if (tab._window && tab._window.focus) {
         tab._window.focus();
+      }
       tab.focus();
       // Focus on tab content
       // TODO: window.focus() is missing on 1.9.2pre
-      if (tab._window && tab._window.content)
+      if (tab._window && tab._window.content) {
         tab._window.content.focus();
+      }
     }
   },
 
-  preview: function( pblock, tab ) {
+  preview: function(pblock, tab) {
     var tabName = tab.text;
 
     if (tabName == this._cacheKey) {
       pblock.innerHTML = this._cacheValue;
     }
-    else if( tabName.length > 1 ){
-      var tab = Utils.tabs.get(tabName);
-      if (tab) {
-        var imgData = CmdUtils.getTabSnapshot( tab, {width:500} );
-        pblock.innerHTML = "Changes to <b style=\"color:yellow\">%s</b> tab.".replace(/%s/, tabName);
-        pblock.innerHTML += "<br/><img src='%s'>".replace(/%s/, imgData );
-      }
-      else
-        pblock.innerHTML = "Switch to tab by name.";
+    else if (tabName.length > 1 && (tab = Utils.tabs.get(tabName))) {
+      var imgData = CmdUtils.getTabSnapshot(tab, {width:500});
+      pblock.innerHTML = "Changes to <b style=\"color:yellow\">%s</b> tab."
+                         .replace(/%s/, tabName);
+      pblock.innerHTML += "<br/><img src='%s'>".replace(/%s/, imgData);
     }
-    else
+    else {
       pblock.innerHTML = "Switch to tab by name.";
+    }
 
     this._cacheKey = tabName;
     this._cacheValue = pblock.innerHTML;
@@ -144,33 +138,31 @@ CmdUtils.CreateCommand({
   name: "close-tab",
   takes: {"tab name": noun_type_tab},
   icon: "chrome://ubiquity/skin/icons/tab_delete.png",
-  description: "Closes the tab that matches the given name.",
-  execute: function( directObj ) {
+  description: "Closes the tab that matches the given name, "
+             + "or if current tab if no name is supplied",
+  execute: function(directObj) {
     var tabName = directObj.text;
     var tab = Utils.tabs.get(tabName);
-    if(tab){
+    if (tabName && tab) {
       tab.close();
-    }else{
+    }
+    else {
       Application.activeWindow.activeTab.close();
     }
     displayMessage(tabName + " tab closed");
   },
 
-  preview: function( pblock, directObj ) {
-    var tabName = directObj.text;
-
-    if( tabName.length > 1 ) {
-      var tab = Utils.tabs.get(tabName);
-      if (tab) {
-        var imgData = CmdUtils.getTabSnapshot( tabs[tabName], {width:500} );
-        pblock.innerHTML = "Closes the <b style=\"color:yellow\">%s</b> tab.".replace(/%s/, tabName);
-        pblock.innerHTML += "<br/><img src='%s'>".replace(/%s/, imgData );
-      }
-      else
-        pblock.innerHTML = "Closes the tab by name.";
+  preview: function(pblock, tab) {
+    var tabName = tab.text;
+    if(tabName.length > 1 && (tab = Utils.tabs.get(tabName))) {
+      var imgData = CmdUtils.getTabSnapshot(tab, {width:500});
+      pblock.innerHTML = "Closes the <b style=\"color:yellow\">%s</b> tab."
+                         .replace(/%s/, tabName);
+      pblock.innerHTML += "<br/><img src='%s'>".replace(/%s/, imgData);
     }
-    else
+    else {
       pblock.innerHTML = "Closes the tab by name.";
+    }
   }
 });
 
@@ -181,39 +173,45 @@ CmdUtils.CreateCommand({
   takes: {"related word": noun_arb_text},
   icon: "chrome://ubiquity/skin/icons/tab_delete.png",
   description: "Closes all open tabs that have the given word in common.",
-  preview: function( pblock, directObj ) {
+  preview: function(pblock, directObj) {
     var query = directObj.text;
     var relatedWord = query.toLowerCase();
     var html = null;
-    if(relatedWord.length != 0){
-      html = "Closes the following tabs that are related to <b style=\"color:yellow\">\"" + relatedWord + "\"</b> : <ul>";
+    if(relatedWord.length != 0) {
+      html = "Closes the following tabs that are related to "
+           + "<b style=\"color:yellow\">\"" + relatedWord + "\"</b> : <ul>";
       var numTabs = 0;
 
-      Application.activeWindow.tabs.forEach(function(tab){
-        if ( tab.uri.spec.toLowerCase().match(relatedWord) || tab.document.title.toLowerCase().match(relatedWord)){
+      Application.activeWindow.tabs.forEach(function(tab) {
+        if (tab.uri.spec.toLowerCase().match(relatedWord) || 
+            tab.document.title.toLowerCase().match(relatedWord)) {
       	  html += "<li>" + tab.document.title + "</li>";
       	  numTabs++;
         }
-      });
+      } );
 
-      if(numTabs == 0){
-        html = "No tabs related to <b style=\"color:yellow\">\"" + relatedWord + "\"</b>";
-      }else{
+      if(numTabs == 0) {
+        html = "No tabs related to <b style=\"color:yellow\">\""
+             + relatedWord + "\"</b>";
+      }
+      else {
         html += "</ul>";
       }
-    }else{
+    }
+    else {
       html = "Closes all tabs related to the word";
     }
-    jQuery(pblock).html( html );
+    jQuery(pblock).html(html);
   },
 
-  execute: function( directObj ) {
+  execute: function(directObj) {
     var query = directObj.text;
     var relatedWord = query.toLowerCase();
     var numTabs = 0;
 
-    Application.activeWindow.tabs.forEach(function(tab){
-      if ( tab.uri.spec.toLowerCase().match(relatedWord) || tab.document.title.toLowerCase().match(relatedWord)){
+    Application.activeWindow.tabs.forEach(function(tab) {
+      if (tab.uri.spec.toLowerCase().match(relatedWord) || 
+          tab.document.title.toLowerCase().match(relatedWord)) {
         tab.close();
         numTabs++;
       }
@@ -229,32 +227,28 @@ function countTabs(filter, noHtml) {
   var count = 0;
   if (filter.length < 1) {
      count = Application.activeWindow.tabs.length;
-  } else {
+  }
+  else {
     Application.activeWindow.tabs.forEach(function(tab) {
       var title = tab.document.title;
       if (title.toLowerCase().indexOf(filter.toLowerCase()) != -1)
         count++;
     });
   }
-  if (count == 1) {
-    if (noHtml) {
-      var previewTemplate = "${count} tab"
-                          + "{if filter} matching '${filter}'{else} total{/if}.";
-    } else {
-      var previewTemplate = "<b>${count}</b> tab"
-                          + "{if filter} matching <i>${filter}</i>{else} total{/if}.";
+  var tabTemplate = "tab";
+  if (count > 1) {
+    tabTemplate += "s";
+  }
+  if (noHtml) {
+    var previewTemplate = "${count} "+tabTemplate
+                        + "{if filter} matching '${filter}'{else} total{/if}.";
     }
-  } else {
-    if (noHtml) {
-      var previewTemplate = "${count} tabs"
-                          + "{if filter} matching '${filter}'{else} total{/if}.";
-    } else {
-       var previewTemplate = "<b>${count}</b> tabs"
-                          + "{if filter} matching <i>${filter}</i>{else} total{/if}.";
-    }
+  else {
+    var previewTemplate = "<b>${count}</b> "+tabTemplate
+                        + "{if filter} matching <i>${filter}</i>{else} total{/if}.";
   }
   var previewData = {
-    count: count,
+    count:  count,
     filter: filter
   };
   return CmdUtils.renderTemplate(previewTemplate, previewData);
@@ -279,10 +273,9 @@ CmdUtils.CreateCommand({
 CmdUtils.CreateCommand({
   name: "refresh",
   description: "Refresh current document",
-  preview: function( pblock ) {pblock.innerHTML=this.description},
   execute: function() {
     var win = CmdUtils.getWindow();
-    win.location.reload( true );
+    win.location.reload(true);
   }
 });
 
@@ -290,13 +283,15 @@ CmdUtils.CreateCommand({
 CmdUtils.CreateCommand({
   name: "bookmark",
   description: "Add current document to bookmarks",
-  preview: function( pblock ) {pblock.innerHTML=this.description},
   execute: function() {
     var win = CmdUtils.getWindowInsecure();
     var doc = CmdUtils.getDocument();
     try {
-      win.sidebar.addPanel(doc.title, win.location.href,"");
-    } catch ( e ) { displayMessage("Page could not be bookmarked!" + ((e)?" - "+e:"")); }
+      win.sidebar.addPanel(doc.title, win.location.href, "");
+    }
+    catch (e) {
+      displayMessage("Page could not be bookmarked!" + ((e) ? " - "+e : "" ));
+    }
   }
 });
 
@@ -304,7 +299,6 @@ CmdUtils.CreateCommand({
 CmdUtils.CreateCommand({
   name: "print",
   description: "Print current page",
-  preview: function( pblock ) {pblock.innerHTML=this.description},
   execute: function() {
     var win = CmdUtils.getWindow();
     win.print();
@@ -317,13 +311,14 @@ CmdUtils.CreateCommand({
   description: "Go back in history",
   takes: {steps: noun_type_number},
 
-  preview: function( pblock, steps ) {
-    var template = "Go back ${steps} {if steps == '1'} step {else} steps {/if} in history";
-    pblock.innerHTML=CmdUtils.renderTemplate(template, {"steps": steps.text});
+  preview: function(pblock, steps) {
+    var template = "Go back ${steps} {if steps == '1'} "
+                 + "step {else} steps {/if} in history";
+    pblock.innerHTML = CmdUtils.renderTemplate(template, {"steps": steps.text});
   },
   execute: function(steps) {
     var win = CmdUtils.getWindow();
-    win.history.go(-this._parseSteps(steps.text));
+    win.history.go(-Math.abs(parseInt(steps.text)));
   }
 });
 
@@ -333,13 +328,14 @@ CmdUtils.CreateCommand({
   description: "Go forward in history",
   takes: {steps: noun_type_number},
 
-  preview: function( pblock, steps ) {
-    var template = "Go forward ${steps} {if steps == '1'} step {else} steps {/if} in history";
-    pblock.innerHTML=CmdUtils.renderTemplate(template, {"steps": steps.text});
+  preview: function(pblock, steps) {
+    var template = "Go forward ${steps} {if steps == '1'} "
+                 + "step {else} steps {/if} in history";
+    pblock.innerHTML = CmdUtils.renderTemplate(template, {"steps": steps.text});
   },
   execute: function(steps) {
     var win = CmdUtils.getWindow();
-    win.history.go(this._parseSteps(steps.text));
+    win.history.go(Math.abs(parseInt(steps.text)));
   }
 });
 
@@ -347,7 +343,6 @@ CmdUtils.CreateCommand({
 CmdUtils.CreateCommand({
   name: "home",
   description: "Go to home page",
-  preview: function( pblock ) {pblock.innerHTML=this.description},
   execute: function() {
     var win = CmdUtils.getWindow();
     win.home();
@@ -359,33 +354,37 @@ CmdUtils.CreateCommand({
 // -----------------------------------------------------------------
 
 
-function setFullPageZoom( level ) {
-  var navigator1 = window
-                    .QueryInterface(Components.interfaces.nsIInterfaceRequestor)
-                    .getInterface(Components.interfaces.nsIWebNavigation);
+function setFullPageZoom(level) {
+  var navigator1 = window.
+                   QueryInterface(Components.interfaces.nsIInterfaceRequestor).
+                   getInterface(Components.interfaces.nsIWebNavigation);
   var docShell = navigator1.QueryInterface(Components.interfaces.nsIDocShell);
-  var docviewer = docShell.contentViewer.QueryInterface(Components.interfaces.nsIMarkupDocumentViewer);
+  var docviewer = docShell.
+                  contentViewer.
+                  QueryInterface(Components.interfaces.nsIMarkupDocumentViewer);
   docviewer.fullZoom = level;
 }
 
 CmdUtils.CreateCommand({
-  name:"zoom",
-  takes:{"percentage": noun_type_percentage},
+  name: "zoom",
+  takes: {"percentage": noun_type_percentage},
   icon: "chrome://ubiquity/skin/icons/magnifier.png",
-  description:"Zooms the Firefox window in or out.",
-  preview:function(pBlock, directObj) {
-    if (directObj.text)
-      pBlock.innerHTML = "Zooms the Firefox window to " +
-        directObj.text + " of its normal size.";
-    else
-      pBlock.innerHTML = "Zooms the Firefox window to a given percentage " +
-      "of its normal size.";
+  description: "Zooms the Firefox window in or out.",
+  preview: function(pBlock, directObj) {
+    var replacement = "a given percentage"; 
+    if (directObj.text) {
+      replacement = directObj.text;
+    }
+    pBlock.innerHTML = "Zooms the Firefox window to " + replacement 
+                       + " of its normal size.";
   },
-  execute:function(directObj) {
-    if (directObj.data)
+  execute: function(directObj) {
+    if (directObj.data) {
       setFullPageZoom(directObj.data);
-    else
+    }
+    else {
       displayMessage("You must provide a percentage to zoom to.");
+    }
   }
 });
 
@@ -396,8 +395,8 @@ CmdUtils.CreateCommand({
 // http://blog.stevenlevithan.com/archives/faster-trim-javascript
 function trim (str) {
   var str = str.replace(/^\s\s*/, ''),
-    ws = /\s/,
-    i = str.length;
+      ws  = /\s/,
+      i   = str.length;
   while (ws.test(str.charAt(--i)));
   return str.slice(0, i + 1);
 }
@@ -406,14 +405,14 @@ function trim (str) {
 CmdUtils.CreateCommand({
   name: "tag",
   homepage: "http://autonome.wordpress.com/",
-  author: { name: "Dietrich Ayala", email: "dietrich@mozilla.com"},
+  author: {name: "Dietrich Ayala", email: "dietrich@mozilla.com"},
   license: "MPL/GPL/LGPL",
   takes : {"text" : noun_arb_text},
   icon: "chrome://mozapps/skin/places/tagContainerIcon.png",
   description: "Adds a tag to describe the current page",
   preview: function(aEl, aTagsString) {
-    aEl.innerHTML = "Describe the current page with tags";
-    aEl.innerHTML += aTagsString.text.length ? " (" + aTagsString.text + ")" : ".";
+    aEl.innerHTML = "Describe the current page with tags"
+                  + aTagsString.text.length ? " (" + aTagsString.text + ")" : ".";
   },
   execute: function(aTagsString) {
     var Cc = Components.classes;
@@ -425,7 +424,8 @@ CmdUtils.CreateCommand({
 
     Utils.reportInfo("URI: " + doc.location);
 
-    var iosvc = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
+    var iosvc = Cc["@mozilla.org/network/io-service;1"].
+                getService(Ci.nsIIOService);
     var currentURI = iosvc.newURI(doc.location, null, null);
 
     var bookmarks = Cc["@mozilla.org/browser/nav-bookmarks-service;1"].
@@ -458,50 +458,50 @@ CmdUtils.CreateCommand({
 // -----------------------------------------------------------------
 
 
-function _getExtensionInfo( Application ){
+function _getExtensionInfo(Application) {
   var extensions = {};
-  Application.extensions.all.forEach(function( ext ){
+  Application.extensions.all.forEach(function(ext) {
     extensions[ext.name] = {
-      version: ext.version,
+      version:  ext.version,
       firstRun: ext.firstRun,
-      enabled: ext._enabled
+      enabled:  ext._enabled
     };
   });
   return extensions;
 }
 
-function _getBrowserInfo( Application ){
+function _getBrowserInfo(Application) {
   var numTabs = 0;
-  Application.windows.forEach(function(win){
+  Application.windows.forEach(function(win) {
     numTabs += win.tabs.length;
   });
 
   var nav = CmdUtils.getWindow().navigator;
 
   return {
-    name: Application.name,
-    version: Application.version,
+    name:            Application.name,
+    version:         Application.version,
     numberOfWindows: Application.windows.length,
-    numberOfTabs: numTabs,
-    cookieEnabled: nav.cookieEnabled,
-    language: nav.language,
-    buildID: nav.buildID
+    numberOfTabs:    numTabs,
+    cookieEnabled:   nav.cookieEnabled,
+    language:        nav.language,
+    buildID:         nav.buildID
   };
 }
 
-function _getOSInfo(){
+function _getOSInfo() {
   var hostJS = CmdUtils.getWindow().navigator;
 
   return {
-    oscpu: hostJS.oscpu,
+    oscpu:    hostJS.oscpu,
     platform: hostJS.platform
   };
 }
 
-function _getPluginInfo(){
+function _getPluginInfo() {
   var plugins = [];
   var hostJS = CmdUtils.getWindow().navigator;
-  for( var i=0; i < hostJS.plugins.length; i++){
+  for (var i=0; i < hostJS.plugins.length; i++) {
     plugins.push({
       name: hostJS.plugins[i].name,
       //description: hostJS.plugins[i].description,
@@ -511,9 +511,10 @@ function _getPluginInfo(){
   return plugins;
 }
 
-function _getErrorInfo(){
-  var consoleService = Components.classes["@mozilla.org/consoleservice;1"]
-      .getService(Components.interfaces.nsIConsoleService);
+function _getErrorInfo() {
+  var consoleService = Components.
+                       classes["@mozilla.org/consoleservice;1"].
+                       getService(Components.interfaces.nsIConsoleService);
 
   // Get the last five errors
   var errors = {};
@@ -522,8 +523,8 @@ function _getErrorInfo(){
   errors = errors.value.slice(-5);
 
   var errorList = [];
-  errors.forEach(function(error){
-    errorList.push( error.message );
+  errors.forEach(function(error) {
+    errorList.push(error.message);
   });
 
   return errorList;
@@ -533,20 +534,20 @@ function _getErrorInfo(){
 // I'm leaving it here so that it gets pushed out to users
 // now. -Aza
 CmdUtils.CreateCommand({
-  name:"report-bug",
-  help : "Inserts Ubiquity debug information at the cursor. Use it when reporting bugs.",
-  _getDebugInfo: function(Application){
+  name: "report-bug",
+  description: "Inserts Ubiquity debug information at the cursor. "
+             + "Use it when reporting bugs.",
+  _getDebugInfo: function(Application) {
     return {
-      browser: _getBrowserInfo(Application),
+      browser:    _getBrowserInfo(Application),
       extensions: _getExtensionInfo(Application),
-      plugins: _getPluginInfo(),
-      os: _getOSInfo(),
-      errors: _getErrorInfo()
+      plugins:    _getPluginInfo(),
+      os:         _getOSInfo(),
+      errors:     _getErrorInfo()
     };
   },
-  preview: "Inserts Ubiquity debug information at the cursor. Use it when reporting bugs.",
-  execute: function(){
+  execute: function() {
     var debug = this._getDebugInfo(Application);
-    CmdUtils.setSelection( Utils.encodeJson(debug) );
+    CmdUtils.setSelection(Utils.encodeJson(debug));
   }
 });
