ef373340cac7146c66ebb87d69ef0c5416c9071e
Atul Varma <varmaa@toolness.com>
2009-03-02 13:19:23 -0800

    The developer commands feed now modifies the HG log page by dynamically mashing-up buildbot information for relevant changesets.

--- ubiquity/standard-feeds/core-developer-commands.js
+++ ubiquity/standard-feeds/core-developer-commands.js
@@ -30,6 +30,67 @@ function addMoreLinks(rev, doc) {
   jQuery("#changesetEntry", doc).append(newRow);
 }
 
+var BUILDBOT_BASE = "https://ubiquity.mozilla.com/buildbot/";
+
+// === {{{getBuildInfo()}}} ===
+//
+// Scrapes a buildbot "one line per build" page and returns an array
+// containing information for each build.
+function getBuildInfo(page) {
+  var builds = [];
+  jQuery("li", page).each(
+    function(i) {
+      var revRegexp = /.*rev=\[([0-9a-f]+)\].*/;
+      builds.push({rev: jQuery(this).text().match(revRegexp)[1],
+                   href: (BUILDBOT_BASE +
+                          jQuery("a:not(:first)", this).attr("href")),
+                   isSuccessful: jQuery(".success", this).length > 0
+                  });
+    }
+  );
+  return builds;
+}
+
+// === {{{addBuildbotInfo()}}} ===
+//
+// Mashes up Buildbot data with the given HG log page.
+
+function addBuildbotInfo(doc) {
+  jQuery(".bigtable tr:first", doc).prepend(
+    '<th class="description">buildbot</th>'
+  );
+  jQuery(".bigtable tr:not(:first)", doc).prepend(
+    '<td class="buildbot"></td>'
+  );
+  jQuery.get(
+    BUILDBOT_BASE + "one_line_per_build",
+    null,
+    function(data) {
+      var builds = getBuildInfo(jQuery(data, doc));
+      builds.forEach(
+        function mashupBuildWithLog(build) {
+          var revUrl = "/hg/ubiquity-firefox/rev/" + build.rev;
+          var revSelector = (".bigtable a[href='" + revUrl + "']");
+          var row = jQuery(revSelector, doc).parent().parent();
+          var status = jQuery("<div>&nbsp;</div>", doc);
+          var bgColor = "#ff0000";
+          var title = "This build may have errors.";
+          if (build.isSuccessful) {
+            bgColor = "#00ff00";
+            title = "This build passed all tests.";
+          }
+          status.css({backgroundColor: bgColor,
+                      width: "1em",
+                      opacity: "0.5"});
+          jQuery(".buildbot", row).append(status);
+          status.wrap('<a title="' + title +
+                      '" href="' + build.href + '"></a>');
+        });
+    },
+    "html"
+  );
+}
+
 // === {{{pageLoad_developerCommands()}}} ===
 //
 // This function is a page-load function that applies transformations
@@ -44,6 +105,8 @@ function pageLoad_developerCommands(doc) {
       var rev = match[1];
       markupTickets(jQuery(".description", doc));
       addMoreLinks(rev, doc);
+    } else if (doc.location.pathname == '/hg/ubiquity-firefox/') {
+      addBuildbotInfo(doc);
     }
   }
 }
