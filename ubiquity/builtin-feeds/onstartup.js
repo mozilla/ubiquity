e393c0c494ed5dcb1b31df615d17ee14baaaf867
Atul Varma <varmaa@toolness.com>
2009-03-05 11:16:06 -0800

    We now automatically allow any page on wiki.mozilla.org with the extension .locked-down.js to be subscribed-to as a locked-down feed. This is done via a pageLoad handler that dynamically adds the appropriate link rel tag when necessary.

--- ubiquity/builtin-feeds/onstartup.js
+++ ubiquity/builtin-feeds/onstartup.js
@@ -2,3 +2,21 @@ function startup_setBasicPreferences() {
   // Allow JS chrome errors to show up in the error console.
   Application.prefs.setValue("javascript.options.showInConsole", true);
 }
+
+function pageLoad_enableLockedDownFeedsInMozillaWiki(doc) {
+  // If a mozilla.org wiki page with the extension .locked-down.js is
+  // in the browser, dynamically insert a <link> element into the page
+  // that tells Ubiquity the page has locked-down commands.
+
+  var loc = doc.location;
+  if (loc.protocol == "https:" &&
+      loc.host == "wiki.mozilla.org" &&
+      loc.pathname.match(/.*\.locked-down\.js$/)) {
+    var link = doc.createElement("link");
+    link.rel = "locked-down-commands";
+    link.href = ("https://wiki.mozilla.org/index.php?title=" +
+                 loc.pathname.slice(1) +
+                 "&action=raw&ctype=text/javascript");
+    doc.documentElement.appendChild(link);
+  }
+}
