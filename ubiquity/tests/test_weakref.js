8cd2e7933ca654bdff39f2cc3062cdcf383640c1
Atul Varma <varmaa@toolness.com>
2009-03-23 16:48:09 -0700

    Made weakref test skip if the xpcom binary component isn't found.

--- ubiquity/tests/test_weakref.js
+++ ubiquity/tests/test_weakref.js
@@ -1,8 +1,14 @@
 Components.utils.import("resource://ubiquity/tests/framework.js");
 
-function test_blah() {
-  var weakref = Components.classes["@labs.mozilla.com/jsweakrefdi;1"]
-                .createInstance(Components.interfaces.nsIJSWeakRef);
+function test_basic_weakref() {
+  var weakref;
+  try {
+    weakref = Components.classes["@labs.mozilla.com/jsweakrefdi;1"]
+              .createInstance(Components.interfaces.nsIJSWeakRef);
+  } catch (e) {
+    throw new this.SkipTestError();
+  }
+
   var obj = new Object();
   weakref.set(obj);
   this.assertEquals(weakref.get(), obj,
