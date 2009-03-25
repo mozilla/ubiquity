ed14c6179bbee248d948b243673fe2cc074c15b6
Atul Varma <varmaa@toolness.com>
2009-03-24 19:40:15 -0700

    Added some very primitive memory tracking code.

---
+++ ubiquity/modules/memory_tracking.js
@@ -0,0 +1,75 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Ubiquity.
+ *
+ * The Initial Developer of the Original Code is Mozilla.
+ * Portions created by the Initial Developer are Copyright (C) 2007
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Atul Varma <atul@mozilla.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+var EXPORTED_SYMBOLS = ["MemoryTracking"];
+
+var classIds = {};
+var trackedItems = {};
+
+var MemoryTracking = {
+  track: function track(obj, name) {
+    var weakref;
+    try {
+      weakref = Components.classes["@labs.mozilla.com/jsweakrefdi;1"]
+                .createInstance(Components.interfaces.nsIJSWeakRef);
+    } catch (e) {
+      // Weakrefs aren't available, do nothing.
+      return;
+    }
+    weakref.set(obj);
+
+    var className = obj.constructor.name;
+    if (name)
+      className += "_" + name;
+    if (typeof(classIds[className]) == "undefined")
+      classIds[className] = 0;
+    var itemName = className + "_" + classIds[className];
+    classIds[className]++;
+    trackedItems[itemName] = weakref;
+  },
+
+  getLiveObjects: function getLiveObjects() {
+    var liveObjects = [];
+
+    for (name in trackedItems) {
+      if (trackedItems[name].get())
+        liveObjects.push(name);
+      else
+        delete trackedItems[name];
+    }
+    return liveObjects;
+  }
+};
