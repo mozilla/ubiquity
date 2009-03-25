a54bff2f62118224c5c6c6dc69ae1d0f6e341d22
Atul Varma <varmaa@toolness.com>
2009-03-24 17:00:50 -0700

    Added another weakref test.

--- ubiquity/tests/test_weakref.js
+++ ubiquity/tests/test_weakref.js
@@ -25,4 +25,29 @@ function test_basic_weakref() {
                     "weakref.get() should be obj.");
 }
 
+function test_GC_works() {
+  var weakref;
+  try {
+    weakref = Components.classes["@labs.mozilla.com/jsweakrefdi;1"]
+              .createInstance(Components.interfaces.nsIJSWeakRef);
+  } catch (e) {
+    throw new this.SkipTestError();
+  }
+
+  var obj = new Object();
+  weakref.set(obj);
+
+  obj = null;
+
+  var debug = Components.classes["@mozilla.org/js/jsd/debugger-service;1"]
+              .getService(Components.interfaces.jsdIDebuggerService);
+  if (!debug.isOn)
+    debug.on();
+  debug.GC();
+
+  this.assertEquals(weakref.get(), null,
+                    "weakref.get() should be null.");
+
+}
+
 exportTests(this);
