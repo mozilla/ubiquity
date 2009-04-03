409097f42f3b485bb66db725502aa4e2a1c58477
Atul Varma <varmaa@toolness.com>
2009-04-03 11:25:47 -0700

    The JS error console is now properly fixed up when the user opens the error console for the first time after errors have been logged.

--- ubiquity/chrome/content/console.js
+++ ubiquity/chrome/content/console.js
@@ -1,47 +1,54 @@
 Components.utils.import("resource://ubiquity/modules/sandboxfactory.js");
 
+function maybeFixUpUbiquityMessage(target) {
+  if (typeof(target.getAttribute) != "function")
+    return;
+  var href = target.getAttribute("href");
+  if (SandboxFactory.isInitialized && href) {
+    if (SandboxFactory.isFilenameReported) {
+      // A command feed's URL had to be "munged" by a sandbox in order to
+      // have XPConnect wrappers implicitly made for it; let's "un-munge" it
+      // here so that it looks intelligible to end-users.
+
+      var prefix = SandboxFactory.protectedFileUriPrefix;
+      if (prefix && href.indexOf(prefix) == 0)
+        target.setAttribute("href", href.slice(prefix.length));
+    } else if (href == SandboxFactory.fileUri) {
+      // We're in an older version of the platform that doesn't allow
+      // code executed in a sandbox to have its file URI specified,
+      // which means error reports will point to the file that the
+      // sandbox call occurred in, rather than the file the code is
+      // in.  Let's at least let the user know about this and tell
+      // them what to do to get accurate information.
+
+      target.setAttribute(
+        "msg",
+        (target.getAttribute("msg") + "\n\n" +
+         "This " + target.getAttribute("type") + " may have occurred " +
+         "in a Ubiquity command feed, but the source " +
+         "file and line number reported below may be incorrect. To " +
+         "receive accurate information, please consider upgrading to " +
+         "the latest beta of Firefox 3.1.")
+      );
+    }
+  }
+}
+
 window.addEventListener(
   "load",
   function() {
     var box = document.getElementById("ConsoleBox");
     box.addEventListener(
       "DOMNodeInserted",
-      function(aEvt) {
-        var target = aEvt.originalTarget;
-        if (typeof(target.getAttribute) != "function")
-          return;
-        var href = target.getAttribute("href");
-        if (SandboxFactory.isInitialized && href) {
-          if (SandboxFactory.isFilenameReported) {
-            // A command feed's URL had to be "munged" by a sandbox in order to
-            // have XPConnect wrappers implicitly made for it; let's "un-munge" it
-            // here so that it looks intelligible to end-users.
-
-            var prefix = SandboxFactory.protectedFileUriPrefix;
-            if (prefix && href.indexOf(prefix) == 0)
-              target.setAttribute("href", href.slice(prefix.length));
-          } else if (href == SandboxFactory.fileUri) {
-            // We're in an older version of the platform that doesn't allow
-            // code executed in a sandbox to have its file URI specified,
-            // which means error reports will point to the file that the
-            // sandbox call occurred in, rather than the file the code is
-            // in.  Let's at least let the user know about this and tell
-            // them what to do to get accurate information.
-
-            target.setAttribute(
-              "msg",
-              (target.getAttribute("msg") + "\n\n" +
-               "This " + target.getAttribute("type") + " may have occurred " +
-               "in a Ubiquity command feed, but the source " +
-               "file and line number reported below may be incorrect. To " +
-               "receive accurate information, please consider upgrading to " +
-               "the latest beta of Firefox 3.1.")
-            );
-          }
-        }
-      },
+      function(aEvt) { maybeFixUpUbiquityMessage(aEvt.originalTarget); },
       true
     );
+
+    var child = box.mConsoleRowBox.firstChild;
+    while (child) {
+      maybeFixUpUbiquityMessage(child);
+      child = child.nextSibling;
+    }
   },
   false
 );
