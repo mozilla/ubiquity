f7ff6fdf66dd17e573e498b1b075b23b77a6d79c
Atul Varma <varmaa@toolness.com>
2009-04-03 13:56:19 -0700

    Removed nsJSWeakRef.  Since it only works w/ the version of the platform that it was compiled for and it's only really useful for debugging ubiquity core, I think it'll be more useful to include it as a separate addon that any extension developer can use (and that they can go through the trouble of finding the right .xpi for their OS/platform version).
    This means that a binary xpcom component now only exists for OS X--nsUbiquityDesktopIntegration.

--- components/src/ubiquitymodule.cpp
+++ components/src/ubiquitymodule.cpp
@@ -1,24 +1,12 @@
 #include "nsIGenericFactory.h"
-#include "nsJSWeakRef.h"
 
 #if XP_MACOSX
 #include "nsUbiquityDesktopIntegration.h"
-#endif
-
-NS_GENERIC_FACTORY_CONSTRUCTOR(nsJSWeakRef)
-
-#if XP_MACOSX
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsUbiquityDesktopIntegration)
 #endif
 
 static nsModuleComponentInfo components[] =
 {
-    {
-        NSJSWEAKREFDI_CLASSNAME,
-        NSJSWEAKREFDI_CID,
-        NSJSWEAKREFDI_CONTRACTID,
-        nsJSWeakRefConstructor,
-    },
 #if XP_MACOSX
     {
         NSUBIQUITYDI_CLASSNAME,
--- ubiquity/tests/test_all.js
+++ ubiquity/tests/test_all.js
@@ -51,7 +51,6 @@ Components.utils.import("resource://ubiquity/tests/test_eventhub.js");
 Components.utils.import("resource://ubiquity/tests/test_suggestion_memory.js");
 Components.utils.import("resource://ubiquity/tests/test_annotation_memory.js");
 Components.utils.import("resource://ubiquity/tests/test_hiddenbrowser.js");
-Components.utils.import("resource://ubiquity/tests/test_weakref.js");
 Components.utils.import("resource://ubiquity/tests/test_parser.js");
 Components.utils.import("resource://ubiquity/tests/test_tag_command.js");
 Components.utils.import("resource://ubiquity/tests/testing_stubs.js");
