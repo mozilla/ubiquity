d04d9475cf6b05885c05f44fd0679454e442c08c
Michael Yoshitaka Erlewine <mitcho@mitcho.com>
2009-04-10 18:19:47 +0900

    updating nountypes.js from bitbucket

--- ubiquity/modules/parser/tng/nountypes.js
+++ ubiquity/modules/parser/tng/nountypes.js
@@ -5,17 +5,91 @@ var nounCache = {};
 // set up noun type detectors
 
 var nounTypes = {
-  arb:      { score: function(x) {return 0.7;} },
-  contact:  { list: ['John','Mary','Bill'],
-    score: function(x) { return (x.search(eval('/^('+matchString(this.list)+')$/i')) >= 0 ? 1 : 0) } },
-  city:     { list: ['San Francisco', 'Tokyo', 'Boston'],
-    score: function(x) { return (x.search(eval('/^('+matchString(this.list)+')$/i')) >= 0 ? 1 : 0) } },
-  time:     { score: function(x) {return (x.search(/^\d+ ?\w+$/i) >= 0 ? 1 : 0);} },
-  number:   { score: function(x) { return (x.search(/^\d+$/) >= 0 ? 1 : 0) } },
-  service:  { list: ['Google', 'Yahoo', 'calendar'],
-    score: function(x) { return (x.search(eval('/^('+matchString(this.list)+')$/i')) >= 0 ? 1 : 0) } },
-  language: { list: ['English','French','Japanese','Chinese'],
-    score: function(x) { return (x.search(eval('/^('+matchString(this.list)+')$/i')) >= 0 ? 1 : 0) } }
+  arb:      { suggest: function(x) [ { text:x, html:x, score:0.7 } ] },
+  contact:  {
+    list: ['John','Mary','Bill'],
+    suggest: function(x) { 
+      let suggestions = [];
+      x = x.toLowerCase();
+      for each (let candidate in this.list) {
+        if (x == candidate.toLowerCase()) {
+          suggestions.push({ text:candidate, html:candidate, score:1 });
+          continue;
+        }
+        if (candidate.toLowerCase().indexOf(x) > -1) {
+          suggestions.push({ text:candidate, html:candidate, score:0.9 });
+          continue;
+        }
+      }
+      return suggestions;
+    }
+  },
+  city:  {
+    list: ['San Francisco', 'San Diego', 'Tokyo', 'Boston'],
+    suggest: function(x) { 
+      let suggestions = [];
+      x = x.toLowerCase();
+      for each (let candidate in this.list) {
+        if (x == candidate.toLowerCase()) {
+          suggestions.push({ text:candidate, html:candidate, score:1 });
+          continue;
+        }
+        if (candidate.toLowerCase().indexOf(x) > -1) {
+          suggestions.push({ text:candidate, html:candidate, score:0.9 });
+          continue;
+        }
+      }
+      return suggestions;
+    }
+  },
+  time:   { suggest: function(x) {
+    if (x.search(/^\d+ ?\w+$/i) >= 0) 
+      return [ { text:x, html:x, score:1 } ];    
+    else
+      return [];
+  } },
+  number: { suggest: function(x) {
+    if (x.search(/^\d+$/) >= 0) 
+      return [ { text:x, html:x, score:1 } ];    
+    else
+      return [];
+  } },
+  service:  {
+    list: ['Google', 'Yahoo', 'Calendar'],
+    suggest: function(x) { 
+      let suggestions = [];
+      x = x.toLowerCase();
+      for each (let candidate in this.list) {
+        if (x == candidate.toLowerCase()) {
+          suggestions.push({ text:candidate, html:candidate, score:1 });
+          continue;
+        }
+        if (candidate.toLowerCase().indexOf(x) > -1) {
+          suggestions.push({ text:candidate, html:candidate, score:0.9 });
+          continue;
+        }
+      }
+      return suggestions;
+    }
+  },
+  language:  {
+    list: ['English','French','Japanese','Chinese'],
+    suggest: function(x) { 
+      let suggestions = [];
+      x = x.toLowerCase();
+      for each (let candidate in this.list) {
+        if (x == candidate.toLowerCase()) {
+          suggestions.push({ text:candidate, html:candidate, score:1 });
+          continue;
+        }
+        if (candidate.toLowerCase().indexOf(x) > -1) {
+          suggestions.push({ text:candidate, html:candidate, score:0.9 });
+          continue;
+        }
+      }
+      return suggestions;
+    }
+  }
 };
 
 function detectNounType(x) {
@@ -25,22 +99,22 @@ function detectNounType(x) {
 }
 
 function protoDetectNounType(x) {
-  let returnObj = [];
+  let returnObj = {};
   for (let type in nounTypes) {
-    let score = nounTypes[type].score(x);
-    if (score > 0)
-      returnObj[type] = score;
+    var suggestions = nounTypes[type].suggest(x);
+    for each (suggestion in suggestions) {
+      suggestion.nountype = type;
+    }    
+    if (suggestions.length > 0)
+      returnObj[type] = suggestions;
   }
   return returnObj;
 }
 
 function cacheNounTypes(args) {
   for each (let arg in args) {
-    if (typeof(arg) == 'object')
-      for each (let x in arg)
-        detectNounType(x);
-    else
-      detectNounType(arg);
+    for each (let x in arg)
+      detectNounType(x._substitutedInput);
   }
   return true;
 }
