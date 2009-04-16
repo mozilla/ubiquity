571c892400be308123c746acb357322508345d35
jonathandicarlo@jonathan-dicarlos-macbook-pro.local <none@none>
2009-04-15 19:08:14 -0700

    Oops, this file should have been in my last commit.

---
+++ ubiquity/tests/test_parser2.js
@@ -0,0 +1,13 @@
+Components.utils.import("resource://ubiquity/modules/utils.js");
+Components.utils.import("resource://ubiquity/modules/cmdmanager.js");
+Components.utils.import("resource://ubiquity/modules/nounutils.js");
+//Components.utils.import("resource://ubiquity/modules/parser/tng/namespace.js");
+
+Components.utils.import("resource://ubiquity/tests/framework.js");
+
+const EXPORTED_SYMBOLS = [];
+
+function testObviousness() {
+  this.assert( true );
+}
+exportTests(this);
