3827967b58b53fb8330e64364139bab79669aecf
Atul Varma <varmaa@toolness.com>
2009-05-10 10:50:20 -0700

    Added a jQuery setup function.

--- ubiquity/jetpack/content/js/jetpack.js
+++ ubiquity/jetpack/content/js/jetpack.js
@@ -1,7 +1,24 @@
-// Just some sample code.
 Components.utils.import("resource://jetpack/modules/jetpack_feed_plugin.js");
 Components.utils.import("resource://jetpack/modules/init.js");
 Components.utils.import("resource://ubiquity/modules/sandboxfactory.js");
+
+// This sets up jQuery when it's loaded in a hidden chrome window
+// that doesn't provide a user interface.
+
+jQuery.ajaxSetup(
+  {xhr: function() {
+     // This is a fix for Ubiquity bug #470. We're going to create the
+     // XHR object from whatever current window the user's using, so
+     // that any UI that needs to be brought up as a result of the XHR
+     // is shown to the user, rather than being invisible and locking
+     // up the application.
+
+     var jsm = {};
+     Components.utils.import("resource://ubiquity/modules/utils.js", jsm);
+     var window = jsm.Utils.currentChromeWindow;
+     return new window.XMLHttpRequest();
+   }
+  });
 
 if (JetpackFeedManager) {
   var watcher = new EventHubWatcher(JetpackFeedManager);
