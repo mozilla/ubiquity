4f3c7eb83eaf22572016b38100c1a485c8e62d5b
Atul Varma <varmaa@toolness.com>
2009-06-02 15:55:48 -0700

    Replaced w/ jetpack's manage.py, tailored to ubiq's path. Removed jetpack-specific paver tasks. the 'test' target currently throws a NotImplementedError(), which we'll fix soon.

--- manage.py
+++ manage.py
@@ -1,22 +1,43 @@
+#! /usr/bin/env python
+
+import os
+import sys
+
+if __name__ == '__main__':
+    # This code is run if we're executed directly from the command-line.
+
+    myfile = os.path.abspath(__file__)
+    mydir = os.path.dirname(myfile)
+    sys.path.insert(0, os.path.join(mydir, 'python-modules'))
+
+    args = sys.argv[1:]
+    if not args:
+        args = ['help']
+
+    # Have paver run this very file as its pavement script.
+    args = ['-f', myfile] + args
+
+    import paver.tasks
+    paver.tasks.main(args)
+    sys.exit(0)
+
+# This code is run if we're executed as a pavement script by paver.
+
 import os
 import sys
 import xml.dom.minidom
-import subprocess
-import shutil
 import zipfile
 import shutil
 import distutils.dir_util
+import time
+import threading
 from ConfigParser import ConfigParser
+
+from paver.easy import *
 
 # Path to the root of the extension, relative to where this script is
 # located.
 EXT_SUBDIR = "ubiquity"
-
-# Full path to xpcshell; if it's not an absolute path, it's assumed
-# to be on the user's PATH.
-g_xpcshell_path = "xpcshell"
-
-g_mydir = os.path.abspath(os.path.split(__import__("__main__").__file__)[0])
 
 def clear_dir(dirname):
     if os.path.exists(dirname) and os.path.isdir(dirname):
@@ -53,241 +74,160 @@ def find_profile_dir(name):
             path = config.get(profile, "Path")
             if not os.path.isabs(path):
                 path = os.path.join(base_path, path)
-            print "Found profile '%s' at %s." % (name, path)
             return path
-    print "Couldn't find a profile called '%s'." % name
     return None
 
-def get_install_rdf_dom(path_to_extension_root):
-    rdf_path = os.path.join(path_to_extension_root, "install.rdf")
+def get_install_rdf_dom(path_to_ext_root):
+    rdf_path = os.path.join(path_to_ext_root, "install.rdf")
     rdf = xml.dom.minidom.parse(rdf_path)
     return rdf
 
-def get_install_rdf_property(path_to_extension_root, property):
-    rdf = get_install_rdf_dom(path_to_extension_root)
+def get_install_rdf_property(path_to_ext_root, property):
+    rdf = get_install_rdf_dom(path_to_ext_root)
     element = rdf.documentElement.getElementsByTagName(property)[0]
     return element.firstChild.nodeValue
 
-def run_program(args, **kwargs):
-    retval = subprocess.call(args, **kwargs)
-    if retval:
-        print "Process failed with exit code %d." % retval
-        sys.exit(retval)
+def resolve_options(options, ext_subdir = EXT_SUBDIR):
+    if not options.get('profile'):
+        options.profile = 'default'
 
-def run_python_script(args):
-    run_program([sys.executable] + args)
+    options.my_dir = os.path.dirname(os.path.abspath(options.pavement_file))
+    options.profile_dir = find_profile_dir(options.profile)
+    options.path_to_ext_root = os.path.join(options.my_dir, ext_subdir)
 
-def get_xpcom_info():
-    cmdline = [
-        os.path.join(os.path.dirname(g_xpcshell_path),
-	             "run-mozilla.sh"),
-        g_xpcshell_path,
-        os.path.join(g_mydir, "get_xpcom_info.js")
-        ]
-    if not os.path.exists(cmdline[0]):
-        cmdline = cmdline[1:]
-    popen = subprocess.Popen(
-        cmdline,
-        stdout = subprocess.PIPE
+    options.ext_id = get_install_rdf_property(options.path_to_ext_root,
+                                              "em:id")
+
+    options.ext_version = get_install_rdf_property(options.path_to_ext_root,
+                                                   "em:version")
+
+    options.ext_name = get_install_rdf_property(options.path_to_ext_root,
+                                                "em:name")
+
+    if options.profile_dir:
+        options.extension_file = os.path.join(options.profile_dir,
+                                              "extensions",
+                                              options.ext_id)
+        # If cygwin, change the path to windows format so firefox can
+        # understand it.
+        if sys.platform == "cygwin":
+            # TODO: Will this work if path_to_ext_root has spaces in it?
+            file = 'cygpath.exe -w ' + options.path_to_ext_root
+            path = "".join(os.popen(file).readlines())
+            path = path.replace("\n", " ").rstrip()
+            options.firefox_path_to_ext_root = path
+        else:
+            options.firefox_path_to_ext_root = options.path_to_ext_root
+
+def remove_extension(options):
+    if not (options.profile_dir and
+            os.path.exists(options.profile_dir) and
+            os.path.isdir(options.profile_dir)):
+        raise BuildFailure("Can't resolve profile directory; aborting.")
+
+    files_to_remove = ["compreg.dat", "xpti.dat"]
+    for filename in files_to_remove:
+        abspath = os.path.join(options.profile_dir, filename)
+        if os.path.exists(abspath):
+            os.remove(abspath)
+    if os.path.exists(options.extension_file):
+        if os.path.isdir(options.extension_file):
+            shutil.rmtree(options.extension_file)
+        else:
+            os.remove(options.extension_file)
+
+INSTALL_OPTIONS = [("profile=", "p", "Profile name.")]
+JSBRIDGE_OPTIONS = [("port=", "p", "Port to use for jsbridge communication."),
+                    ("binary=", "b", "Path to Firefox binary.")]
+
+@task
+@cmdopts(INSTALL_OPTIONS)
+def install(options):
+    """Install the extension to a Firefox profile."""
+
+    resolve_options(options)
+    remove_extension(options)
+
+    extdir = os.path.dirname(options.extension_file)
+    if not os.path.exists(extdir):
+        distutils.dir_util.mkpath(extdir)
+    fileobj = open(options.extension_file, "w")
+    fileobj.write(options.firefox_path_to_ext_root)
+    fileobj.close()
+    print "Extension '%s' installed to profile '%s'." % (options.ext_id,
+                                                         options.profile)
+
+@task
+@cmdopts(INSTALL_OPTIONS)
+def uninstall(options):
+    """Uninstall the extension from a Firefox profile."""
+
+    resolve_options(options)
+    remove_extension(options)
+    print "Extension '%s' uninstalled from profile '%s'." % (options.ext_id,
+                                                             options.profile)
+
+@task
+def xpi(options):
+    """Build a distributable xpi installer for the extension."""
+
+    resolve_options(options)
+
+    zfname = "%s-%s.xpi" % (options.ext_name.lower(), options.ext_version)
+    zf = zipfile.ZipFile(zfname, "w", zipfile.ZIP_DEFLATED)
+    for dirpath, dirnames, filenames in os.walk(options.path_to_ext_root):
+        for filename in filenames:
+            abspath = os.path.join(dirpath, filename)
+            arcpath = abspath[len(options.path_to_ext_root)+1:]
+            zf.write(abspath, arcpath)
+    print "Created %s." % zfname
+
+def start_jsbridge(options):
+    import mozrunner
+    import jsbridge
+
+    resolve_options(options)
+
+    if not options.get('port'):
+        options.port = '24242'
+    options.port = int(options.port)
+    options.binary = options.get('binary')
+
+    plugins = [jsbridge.extension_path, options.path_to_ext_root]
+    profile = mozrunner.FirefoxProfile(
+        plugins=plugins,
+        preferences={'browser.startup.homepage' : 'about:blank',
+                     'startup.homepage_welcome_url' : 'about:blank'}
         )
-    retval = popen.wait()
-    assert retval == 0
-    os_target, xpcomabi = popen.stdout.read().splitlines()
-    comsd = os.path.join(os.path.dirname(g_xpcshell_path),
-                         "components")
-    return dict(comsd = comsd,
-                os_target = os_target,
-                xpcomabi = xpcomabi)
+    runner = mozrunner.FirefoxRunner(profile=profile,
+                                     binary=options.binary,
+                                     cmdargs=["-jsbridge", str(options.port)])
+    runner.start()
 
-if __name__ == "__main__":
-    args = sys.argv[1:]
-    if not args:
-        print "usage: %s <command>" % sys.argv[0]
-        print
-        print "'command' can be one of the following:"
-        print
-        print "    try - run Firefox w/ new profile and extension installed"
-        print "    unittest - run unit tests"
-        print "    test - run system and unit tests"
-        print "    install - install to the given profile"
-        print "    uninstall - uninstall from the given profile"
-        print "    build-xpi - build an xpi of the addon"
-        print "    build-components - build C++ XPCOM components"
-        print
-        sys.exit(1)
+    back_channel, bridge = jsbridge.wait_and_create_network("127.0.0.1",
+                                                            options.port)
 
-    if os.environ.get("OBJDIR"):
-        g_xpcshell_path = os.path.join(os.environ["OBJDIR"],
-                                       "dist", "bin", g_xpcshell_path)
+    return Bunch(back_channel = back_channel,
+                 bridge = bridge,
+                 runner = runner)
 
-    path_to_extension_root = os.path.join(g_mydir, EXT_SUBDIR)
+@task
+@cmdopts(JSBRIDGE_OPTIONS)
+def run(options):
+    """Run Firefox in a temporary new profile with the extension installed."""
 
-    cmd = args[0]
+    remote = start_jsbridge(options)
 
-    if cmd == "try":
-        import systemtests
-        import jsbridge
-        print ("Starting Firefox with a new profile and "
-               "the extension installed...")
-        moz = jsbridge.start_from_settings(systemtests.settings)
-        print "Firefox started, quit it or press CTRL-C to exit."
-        try:
-            moz.wait()
-        except KeyboardInterrupt:
-            moz.stop()
-        print "Farewell."
-        sys.exit(0)
-    if cmd == "test":
-        this_script = sys.argv[0]
-        this_dir = os.path.dirname(this_script)
-        print "Running unit tests."
-        run_python_script([this_script, "unittest"])
-        print "Running system tests."
-        run_python_script([os.path.join(this_dir, "systemtests.py")])
-        print "All tests successful."
-    elif cmd == "unittest":
-        #if subprocess.call(["which", g_xpcshell_path],
-        #                   stdout=subprocess.PIPE) != 0:
-        #    print "You must have xpcshell on your PATH to run tests."
-        #    sys.exit(1)
+    try:
+        print "Now running, press Ctrl-C to stop."
+        remote.runner.wait()
+    except KeyboardInterrupt:
+        print "Received interrupt, stopping."
+        remote.runner.stop()
 
-        xpcshell_args = [
-            g_xpcshell_path,
-            "-v", "180",     # Use Javascript 1.8
-            "-w",            # Enable warnings
-            "-s",            # Enable strict mode
-            os.path.join(g_mydir, "xpcshell_tests.js"),
-            path_to_extension_root
-            ]
+@task
+@cmdopts(JSBRIDGE_OPTIONS)
+def test(options):
+    """Run test suite."""
 
-        retval = subprocess.call(xpcshell_args)
-        sys.exit(retval)
-    elif cmd in ["install", "uninstall"]:
-        if len(args) != 2:
-            print "Attempting to find location of default profile..."
-
-            profile_dir = find_profile_dir("default")
-        else:
-            profile_dir = args[1]
-            if not os.path.exists(profile_dir):
-                print "Attempting to find a profile with the name '%s'." % (
-                    profile_dir
-                    )
-                profile_dir = find_profile_dir(profile_dir)
-
-        if not (profile_dir and os.path.exists(profile_dir) and
-                os.path.isdir(profile_dir)):
-            print "Can't resolve profile directory; aborting."
-            sys.exit(1)
-
-        extension_id = get_install_rdf_property(path_to_extension_root,
-                                                "em:id")
-
-        extension_file = os.path.join(profile_dir,
-                                      "extensions",
-                                      extension_id)
-        files_to_remove = ["compreg.dat",
-                           "xpti.dat"]
-        for filename in files_to_remove:
-            abspath = os.path.join(profile_dir, filename)
-            if os.path.exists(abspath):
-                os.remove(abspath)
-        if os.path.exists(extension_file):
-            if os.path.isdir(extension_file):
-                shutil.rmtree(extension_file)
-            else:
-                os.remove(extension_file)
-        if cmd == "install":
-            #if cygwin, change the path to windows format so firefox can understand it
-            if sys.platform == "cygwin":
-                file = 'cygpath.exe -w ' + path_to_extension_root
-                path_to_extension_root = "".join(os.popen(file).readlines()).replace("\n", " ").rstrip()
-
-            extdir = os.path.dirname(extension_file)
-            if not os.path.exists(extdir):
-                distutils.dir_util.mkpath(extdir)
-            fileobj = open(extension_file, "w")
-            fileobj.write(path_to_extension_root)
-            fileobj.close()
-            print "Extension '%s' installed." % extension_id
-        else:
-            print "Extension '%s' uninstalled." % extension_id
-    elif cmd == "build-xpi":
-        version = get_install_rdf_property(path_to_extension_root,
-                                           "em:version")
-        extname = get_install_rdf_property(path_to_extension_root,
-                                           "em:name").lower()
-        zfname = "%s-%s.xpi" % (extname, version)
-        zf = zipfile.ZipFile(zfname,
-                             "w",
-                             zipfile.ZIP_DEFLATED)
-        for dirpath, dirnames, filenames in os.walk(path_to_extension_root):
-            for filename in filenames:
-                abspath = os.path.join(dirpath, filename)
-                arcpath = abspath[len(path_to_extension_root)+1:]
-                zf.write(abspath, arcpath)
-        print "Created %s." % zfname
-    elif cmd == "build-components":
-        if "TOPSRCDIR" not in os.environ:
-            print ("Please set the TOPSRCDIR environment variable "
-                   "to the root of your mozilla-central checkout. "
-                   "If you're on Windows, this should be a standard "
-                   "Windows-style path, NOT a unix-style path.")
-            sys.exit(1)
-        if "OBJDIR" not in os.environ:
-            print ("Please set the OBJDIR envirionment variable "
-                   "to the root of your objdir. "
-                   "If you're on Windows, this should be a standard "
-                   "Windows-style path, NOT a unix-style path.")
-            sys.exit(1)
-        xpcominfo = get_xpcom_info()
-        topsrcdir = os.environ["TOPSRCDIR"]
-        objdir = os.environ["OBJDIR"]
-        comp_src_dir = os.path.join(g_mydir, "components")
-        rel_dest_dir = os.path.join("browser", "components", "ubiquity")
-        comp_dest_dir = os.path.join(topsrcdir, rel_dest_dir)
-        comp_xpi_dir = os.path.join(objdir, "dist", "xpi-stage",
-                                    "ubiquity", "components")
-        comp_plat_dir = os.path.join(
-            g_mydir, "ubiquity", "platform",
-            "%(os_target)s_%(xpcomabi)s" % xpcominfo,
-            "components",
-            )
-
-        clear_dir(comp_dest_dir)
-        clear_dir(comp_xpi_dir)
-        clear_dir(comp_plat_dir)
-
-        shutil.copytree(comp_src_dir, comp_dest_dir)
-
-        # Ensure that these paths are unix-like on Windows.
-	sh_pwd = subprocess.Popen(["sh", "-c", "pwd"],
-                                  cwd=topsrcdir,
-                                  stdout=subprocess.PIPE)
-        sh_pwd.wait()
-        unix_topsrcdir = sh_pwd.stdout.read().strip()
-        unix_rel_dest_dir = rel_dest_dir.replace("\\", "/")
-
-        # We're specifying 'perl' here because we have to for this
-        # to work on Windows.
-        run_program(["perl",
-                     os.path.join(topsrcdir, "build", "autoconf",
-                                  "make-makefile"),
-                     "-t", unix_topsrcdir,
-                     unix_rel_dest_dir],
-                    cwd=objdir)
-        run_program(["make"],
-                    cwd=os.path.join(objdir, rel_dest_dir))
-
-        shutil.copytree(comp_xpi_dir, comp_plat_dir)
-        for filename in os.listdir(comp_xpi_dir):
-            shutil.copy(os.path.join(comp_xpi_dir, filename),
-                        xpcominfo["comsd"])
-
-        for filename in ["compreg.dat", "xpti.dat"]:
-            fullpath = os.path.join(xpcominfo["comsd"], filename)
-            if os.path.exists(fullpath):
-                os.unlink(fullpath)
-    else:
-        print "Unknown command '%s'" % cmd
-        sys.exit(1)
+    raise NotImplementedError("Need to implement this!")
