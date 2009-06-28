b141abd524d367ddacec2e75f8b7adfe71c05b25
satyr <murky.satyr@gmail.com>
2009-06-29 02:46:15 +0900

    pageedit: Fixed bugs and ToDos.
    --HG--
    extra : rebase_source : 815a00a8e62967883ddb6e5e31a34aaf991725e1

--- ubiquity/standard-feeds/pageedit.js
+++ ubiquity/standard-feeds/pageedit.js
@@ -1,22 +1,23 @@
+const AS = (Cc["@mozilla.org/browser/annotation-service;1"]
+            .getService(Ci.nsIAnnotationService));
+const IOS = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
+
+const ToRemove = "_toRemove";
 
 CmdUtils.CreateCommand({
   names: ["delete"],
   description: "Deletes the selected chunk of HTML from the page.",
   icon: "chrome://ubiquity/skin/icons/delete.png",
-  execute: function() {
+  execute: function delete_execute() {
     var sel = context.focusedWindow.getSelection();
     var document = context.focusedWindow.document;
-
-    if (sel.rangeCount >= 1) {
-        var range = sel.getRangeAt(0);
-        var newNode = document.createElement("div");
-        newNode.className = "_toRemove";
-        range.surroundContents(newNode);
+    for (var i = sel.rangeCount; i--;) {
+      var range = sel.getRangeAt(i);
+      var newNode = document.createElement("div");
+      newNode.className = ToRemove;
+      range.surroundContents(newNode);
     }
-
-    CmdUtils.loadJQuery(function(jQuery) {
-      jQuery("._toRemove").slideUp();
-    });
+    jQuery("." + ToRemove, document).slideUp();
   }
 });
 
@@ -24,22 +25,31 @@ CmdUtils.CreateCommand({
   names: ["undelete"],
   description: "Restores the HTML deleted by the delete command.",
   icon: "chrome://ubiquity/skin/icons/arrow_undo.png",
-  execute: function() {
-    CmdUtils.loadJQuery(function(jQuery) {
-      jQuery("._toRemove").slideDown();
-    });
+  execute: function undelete_execute() {
+    jQuery("." + ToRemove, context.focusedWindow.document)
+      .slideDown(function onUndelete() {
+        var $div = jQuery(this);
+        $div.after($div.contents()).remove();
+      });
   }
 });
+
+function stopEditingPage() {
+  var doc = CmdUtils.getDocument();
+  doc.body.contentEditable = "false";
+  doc.designMode = "off";
+  return doc;
+}
 
 CmdUtils.CreateCommand({
   names: ["edit page", "turn on edit mode"],
   description: "Puts the web page into a mode where you can edit the contents.",
   help: "In edit mode, you can edit the page like any document: Select text, delete it, add to it, copy and paste it.  Issue \'bold\', \'italic\', or \'underline\' commands to add formatting.  Issue the 'save' command to save your changes so they persist even when you reload the page.  Issue 'stop-editing-page' when you're done to go back to the normal page viewing mode.",
   icon: "chrome://ubiquity/skin/icons/page_edit.png",
-  execute: function() {
-    // TODO: works w/o wrappedJSObject in CmdUtils.getDocumentInsecure() call- fix this
-    CmdUtils.getDocumentInsecure().body.contentEditable = 'true';
-    CmdUtils.getDocumentInsecure().designMode='on';
+  execute: function edit_page_execute() {
+    var doc = CmdUtils.getDocument();
+    doc.body.contentEditable = "true";
+    doc.designMode = "on";
   }
 });
 
@@ -50,10 +60,7 @@ CmdUtils.CreateCommand({
                 "back to normal page viewing. If you want the changes to " +
                 "persist on page reload, issue the 'save' command first.",
   icon: "chrome://ubiquity/skin/icons/page_refresh.png",
-  execute: function() {
-    CmdUtils.getDocumentInsecure().body.contentEditable = 'false';
-    CmdUtils.getDocumentInsecure().designMode='off';
-  }
+  execute: stopEditingPage,
 });
 
 CmdUtils.CreateCommand({
@@ -62,104 +69,33 @@ CmdUtils.CreateCommand({
                "They will persist on page reload. You can remove them " +
                "with the 'undo page edits' command.",
   icon: "chrome://ubiquity/skin/icons/page_save.png",
-  execute: function() {
-    // TODO: works w/o wrappedJSObject in CmdUtils.getDocumentInsecure() call- fix this
-    CmdUtils.getDocumentInsecure().body.contentEditable = 'false';
-    CmdUtils.getDocumentInsecure().designMode = 'off';
-
-    var annotationService = Components.classes["@mozilla.org/browser/annotation-service;1"]
-                            .getService(Components.interfaces.nsIAnnotationService);
-    var ioservice = Components.classes["@mozilla.org/network/io-service;1"]
-                            .getService(Components.interfaces.nsIIOService);
-
-    var body = jQuery( CmdUtils.getDocumentInsecure().body );
-
-    annotationService.setPageAnnotation(ioservice.newURI(window.content.location.href, null, null), "ubiquity/edit", body.html(), 0, 4);
+  execute: function save_edits_execute() {
+    var {location, body} = stopEditingPage();
+    AS.setPageAnnotation(IOS.newURI(location.href, null, null),
+                         "ubiquity/edit", body.innerHTML, 0, 4);
   }
 });
-
-var pageLoad_restorePageAnnotations = function (window) {
-  if (!window.location)
-    return;
-
-  var annotationService = Components.classes["@mozilla.org/browser/annotation-service;1"]
-                          .getService(Components.interfaces.nsIAnnotationService);
-  var ioservice = Components.classes["@mozilla.org/network/io-service;1"]
-                  .getService(Components.interfaces.nsIIOService);
-
-  var uri = ioservice.newURI(window.location.href, null, null);
-
-  var annotationNames = annotationService.getPageAnnotationNames(uri, {});
-
-  for (var i=0; i<annotationNames.length; i++) {
-
-    var annotationName, annotationValue;
-    var startNode, endNode;
-    var startXpath, endXpath;
-    var startOffset, endOffset;
-
-    if (annotationNames[i].match("ubiquity/delete/")) {
-      annotationName = annotationNames[i].substring(16);
-      annotationValue = annotationService.getPageAnnotation(uri, annotationNames[i]);
-
-      // get xpaths out of name
-      startXpath = annotationName.substring(0, annotationName.indexOf("#"));
-      endXpath = annotationName.substring(annotationName.indexOf("#") + 1);
-
-      // get offsets out of value
-      startOffset = parseInt(annotationValue.substring(0, annotationValue.indexOf("#")));
-      endOffset = parseInt(annotationValue.substring(annotationValue.indexOf("#") + 1));
-
-
-      // find the nodes from the xpaths
-      var iterator;
-      iterator = doc.evaluate(startXpath, doc, null, XPathResult.ANY_TYPE, null);
-      startNode = iterator.iterateNext();
-      iterator = doc.evaluate(endXpath, doc, null, XPathResult.ANY_TYPE, null);
-      endNode = iterator.iterateNext();
-
-
-      // delete the text content in between the start and end nodes
-      if (startNode == endNode) {
-        startNode.textContent = startNode.textContent.substring(0, startOffset) +
-          startNode.textContent.substring(endOffset);
-      }
-      else {
-        startNode.textContent = startNode.textContent.substring(0, startOffset);
-        var curNode = startNode.nextSibling;
-        while (curNode && (curNode != endNode)) {
-          curNode.textContent = "";
-          curNode = curNode.nextSibling;
-        }
-        endNode.textContent = endNode.textContent.substring(endOffset);
-      }
-
-    }
-
-    if (annotationNames[i] == "ubiquity/edit") {
-      // TODO: works w/o wrappedJSObject in CmdUtils.getDocumentInsecure() call- fix this
-      var body = jQuery( CmdUtils.getDocumentInsecure().body );
-
-      annotationValue = annotationService.getPageAnnotation(uri, annotationNames[i]);
-      body.html(annotationValue);
-
-      // TODO: Fix "TypeError: head is not defined" on some pages
-
-    }
-  }
-};
 
 // removes all page annotations - add more functionality
 CmdUtils.CreateCommand({
   names: ["undo page edits", "remove annotations"],
   description: "Resets any annotation changes you've made to this page.",
   icon: "chrome://ubiquity/skin/icons/page_delete.png",
-  execute: function() {
-    var annotationService = Components.classes["@mozilla.org/browser/annotation-service;1"]
-                            .getService(Components.interfaces.nsIAnnotationService);
-    var ioservice = Components.classes["@mozilla.org/network/io-service;1"]
-                            .getService(Components.interfaces.nsIIOService);
-    annotationService.removePageAnnotations(ioservice.newURI(window.content.location.href, null, null));
-    window.content.location.reload();
-  }
+  execute: function undo_edits_execute() {
+    var {location} = CmdUtils.getWindow();
+    AS.removePageAnnotations(IOS.newURI(location.href, null, null));
+    location.reload();
+  },
 });
+
+function pageLoad_restorePageAnnotations(document) {
+  if (!document.location) return;
+
+  var uri = IOS.newURI(document.location.href, null, null);
+  AS.getPageAnnotationNames(uri, {}).forEach(function eachAN(annoName) {
+    if (annoName === "ubiquity/edit") {
+      document.body.innerHTML = AS.getPageAnnotation(uri, annoName);
+      // TODO: Fix "TypeError: head is not defined" on some pages
+    }
+  });
+}
