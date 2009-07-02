66fe244b529bcef4e9b46bd11fc905faa439bf74
satyr <murky.satyr@gmail.com>
2009-07-02 09:59:19 +0900

    standard-feeds/map: Fixed a bug where the preview broke with Parser 1.
    --HG--
    extra : rebase_source : f941713cbd686cf6df2df81312e0d610023c985b

--- ubiquity/standard-feeds/map.js
+++ ubiquity/standard-feeds/map.js
@@ -4,63 +4,47 @@ var API_KEY = "ABQIAAAAO0oNFUXoUNx4MuxcPwakNhR3yUCx-o6JvWtDFa7jNOakHN7MrBSTsaKtG
 
 CmdUtils.CreateCommand({
   names: ["map"],
-  arguments: [{role: "object",
-               nountype: noun_type_async_address,
-               label: "address"}],
+  arguments: {object: noun_type_async_address},
   icon: "chrome://ubiquity/skin/icons/map.png",
   description: "Turns an address or location name into a Google Map.",
-  help:"Try issuing &quot;map kalamazoo&quot;.  You can click on the map in the preview pane to get a" +
+  help: "Try issuing &quot;map kalamazoo&quot;.  You can click on the map in the preview pane to get a" +
        " larger, interactive map that you can zoom and pan around.  You can then click the &quot;insert map in page&quot;" +
        " (if you're in an editable text area) to insert the map.  So you can, for example, type an address in an email, " +
        " select it, issue &quot;map&quot;, click on the preview, and then insert the map.",
-  execute: function( arguments ) {
-    if (arguments.object && arguments.object.text) {
-      var location = arguments.object.text;
-      var url = "http://maps.google.com/?q=";
-      url += encodeURIComponent(location);
-
-      Utils.openUrlInBrowser( url );
-    } else {
-      Utils.openUrlInBrowser( "http://maps.google.com" );
-    }
+  execute: function map_execute({object: {text}}) {
+    Utils.openUrlInBrowser("http://maps.google.com/" +
+                           (text ? "?q=" + encodeURIComponent(text) : ""));
   },
   previewUrl: "templates/map.html",
-  preview: function(pblock, arguments) {
-      // TODO: This isn't terribly safe; ideally, we should be communicating
-      // with the other page via DOM events, etc.
-    if (arguments.object && arguments.object.text ){
-      var dobj = arguments.object;
-      var previewWindow = pblock.ownerDocument.defaultView;
-      previewWindow = XPCSafeJSObjectWrapper(previewWindow);
-      previewWindow.Ubiquity.context = context;
+  preview: function map_preview(pblock, {object}) {
+    // TODO: This isn't terribly safe; ideally, we should be communicating
+    // with the other page via DOM events, etc.
+    var previewWindow =
+      XPCSafeJSObjectWrapper(pblock.ownerDocument.defaultView);
+    var {Ubiquity} = previewWindow;
 
-      previewWindow.Ubiquity.resizePreview = function(height) {
-        // TODO: Do something to change height of iframe?
-      };
-
-      previewWindow.Ubiquity.insertHtml = function(html) {
-        if (typeof(html) != "string")
-          return;
-        var doc = context.focusedWindow.document;
-        var focused = context.focusedElement;
-
-        if (doc.designMode == "on") {
-          // The "query" here is useful so that you don't have to retype what
-          // you put in the map command. That said, this is map-command
-          // specific and should be factored out. -- Aza
-          doc.execCommand("insertHTML", false, dobj.html + "<br/>" + html);
-        }
-        else if (CmdUtils.getSelection()) {
-	  CmdUtils.setSelection(html);
-      	}
-      	else {
-      	  displayMessage(_("Cannot insert in a non-editable space. Use 'edit page' for an editable page."));
-      	}
-      };
-      previewWindow.Ubiquity.onPreview(dobj);
-    } else {
-      pblock.innerHTML = this.description;
-    }
+    Ubiquity.context = context;
+    Ubiquity.resizePreview = function map_resizePreview(height) {
+      // TODO: Do something to change height of iframe?
+    };
+    Ubiquity.insertHtml = function map_insertHtml(html) {
+      if (typeof html !== "string")
+        return;
+      var doc = context.focusedWindow.document;
+      var focused = context.focusedElement;
+      
+      if (doc.designMode === "on") {
+        // The "query" here is useful so that you don't have to retype what
+        // you put in the map command. That said, this is map-command
+        // specific and should be factored out. -- Aza
+        doc.execCommand("insertHTML", false, object.html + "<br/>" + html);
+      }
+      else if (CmdUtils.getSelection())
+        CmdUtils.setSelection(html);
+      else
+        displayMessage(_("Cannot insert in a non-editable space. " +
+                         "Use 'edit page' for an editable page."));
+    };
+    Ubiquity.onPreview(object);
   }
 });
-
