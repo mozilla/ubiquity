fefe48d3b07f8d5824c355cb865f9b7c874ef62c
Michael Yoshitaka Erlewine <mitcho@mitcho.com>
2009-07-06 01:02:57 +0900

    update to command template (closing #733)

--- ubiquity/chrome/content/command-template.js
+++ ubiquity/chrome/content/command-template.js
@@ -1,13 +1,13 @@
 /* This is a template command. */
 CmdUtils.CreateCommand({
-  name: "example",
+  names: ["example"],
   icon: "http://www.mozilla.com/favicon.ico",
   description: "A short description of your command.",
   help: "How to use your command.",
   author: {name: "Your Name", email: "you@mozilla.com"},
   license: "GPL",
   homepage: "http://labs.mozilla.com/",
-  arguments: {object: noun_arb_text},
+  arguments: [{role: 'object', nountype: noun_arb_text}],
   preview: function preview(pblock, args) {
     pblock.innerHTML = "Your input is <b>" + args.object.html + "</b>.";
   },
