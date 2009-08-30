13964cf481dcfe60548d71fb0ad2e1f9240e8584
satyr <murky.satyr@gmail.com>
2009-08-30 13:44:48 +0900

    standard-feeds/map: Workarounded #886. Removed excess code.

--- ubiquity/standard-feeds/map.js
+++ ubiquity/standard-feeds/map.js
@@ -1,38 +1,33 @@
-var API_KEY = "ABQIAAAAO0oNFUXoUNx4MuxcPwakNhR3yUCx-o6JvWtDFa7jNOakHN7MrBSTsaKtGJjaVMeVURIpTa3cD1qNfA";
-// This API key is for https://people.mozilla.com only."
-// TODO: Update to use Google Maps API V3, which doesn't require a key
-
+// TODO: Update to use Google Maps API V3, which doesn't require a key (#814)
 CmdUtils.CreateCommand({
   names: ["map"],
   arguments: {object: noun_type_geo_address_or_anything},
   icon: "chrome://ubiquity/skin/icons/map.png",
   description: "Turns an address or location name into a Google Map.",
-  help: "Try issuing &quot;map kalamazoo&quot;.  You can click on the map in the preview pane to get a" +
-       " larger, interactive map that you can zoom and pan around.  You can then click the &quot;insert map in page&quot;" +
-       " (if you're in an editable text area) to insert the map.  So you can, for example, type an address in an email, " +
-       " select it, issue &quot;map&quot;, click on the preview, and then insert the map.",
+  help: ("Try issuing &quot;map kalamazoo&quot;.  " +
+         "You can click on the map in the preview pane to get a larger, " +
+         "interactive map that you can zoom and pan around.  " +
+         "You can then click the &quot;insert map in page&quot; " +
+         "(if you're in an editable text area) to insert the map.  " +
+         "So you can, for example, type an address in an email,  " +
+         "select it, issue &quot;map&quot;, click on the preview, " +
+         "and then insert the map."),
   execute: function map_execute({object: {text}}) {
     Utils.openUrlInBrowser("http://maps.google.com/" +
                            (text ? "?q=" + encodeURIComponent(text) : ""));
   },
-  previewUrl: "templates/map.html",
+  previewUrl: (
+    Cu.import("resource://ubiquity/modules/setup.js", null)
+    .UbiquitySetup.getBaseUri() + "standard-feeds/templates/map.html"),
   preview: function map_preview(pblock, {object}) {
     // TODO: This isn't terribly safe; ideally, we should be communicating
     // with the other page via DOM events, etc.
-    var previewWindow =
+    var {Ubiquity} =
       XPCSafeJSObjectWrapper(pblock.ownerDocument.defaultView);
-    var {Ubiquity} = previewWindow;
-
-    Ubiquity.context = context;
-    Ubiquity.resizePreview = function map_resizePreview(height) {
-      // TODO: Do something to change height of iframe?
-    };
     Ubiquity.insertHtml = function map_insertHtml(html, text) {
-      if (typeof html !== "string")
-        return;
+      if (typeof html !== "string") return;
+
       var doc = context.focusedWindow.document;
-      var focused = context.focusedElement;
-      
       if (doc.designMode === "on") {
         // The "query" here is useful so that you don't have to retype what
         // you put in the map command. That said, this is map-command
