02e24e7c6005d532f3fb7f1717fc1c3bb2318245
Michael Yoshitaka Erlewine <mitcho@mitcho.com>
2009-09-02 00:34:13 -0400

    alias: adding experimental alias-commands feed.

---
+++ ubiquity/standard-feeds/alias-commands.js
@@ -0,0 +1,7 @@
+CmdUtils.CreateAlias({
+  names: ["anglicize"],
+  verb: 'translate',
+  arguments: [{role:  "goal",
+               input: "English"}],
+  icon: "resource://ubiquity/chrome/skin/icons/union_jack.ico"
+});
