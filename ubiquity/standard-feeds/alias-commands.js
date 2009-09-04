0ff1e8c022b11f725a048c16e2bccd17fdf2ed0a
Michael Yoshitaka Erlewine <mitcho@mitcho.com>
2009-09-04 15:24:32 -0400

    alias: updating germanize command

--- ubiquity/standard-feeds/alias-commands.js
+++ ubiquity/standard-feeds/alias-commands.js
@@ -1,12 +1,7 @@
-CmdUtils.CreateAlias({
-  names: ["anglicize"],
-  verb: "translate",
-  givenArgs: { goal: { input: "English" } },
-  icon: "resource://ubiquity/chrome/skin/icons/union_jack.ico"
-});
 
 CmdUtils.CreateCommand({
   names: ["germanize"],
+  icon: "resource://ubiquity/chrome/skin/icons/german_flag.png",
   arguments: [{role: "object", nountype: noun_arb_text},
               {role: "source", nountype: noun_type_lang_google}],
   preview: function germanize_preview(pblock, args) {
@@ -17,4 +12,11 @@ CmdUtils.CreateCommand({
     args.goal = { input: "German", summary: "German", data: "de" };
     CmdUtils.executeCommand('translate', args);
   }
+});
+
+CmdUtils.CreateAlias({
+  names: ["anglicize"],
+  verb: "translate",
+  givenArgs: { goal: { input: "English" } },
+  icon: "resource://ubiquity/chrome/skin/icons/union_jack.ico"
 });
