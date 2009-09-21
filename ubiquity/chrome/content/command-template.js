0ded7150fdb6fda88225e01f6f04af09f966bdf4
satyr <murky.satyr@gmail.com>
2009-09-22 06:36:38 +0900

    command-template: Added some more available options.
    --HG--
    extra : rebase_source : e2a118bc361f3a1f0196dd3e5c1a7f3d541fd9b2

--- ubiquity/chrome/content/command-template.js
+++ ubiquity/chrome/content/command-template.js
@@ -1,18 +1,22 @@
 /* This is a template command. */
 CmdUtils.CreateCommand({
   names: ["example"],
-  icon: "http://www.mozilla.com/favicon.ico",
   description: "A short description of your command.",
   help: "How to use your command.",
-  author: {name: "Your Name", email: "you@mozilla.com"},
-  license: "GPL",
-  homepage: "http://labs.mozilla.com/",
-  arguments: [{role: 'object', nountype: noun_arb_text}],
-  preview: function preview(pblock, args) {
-    pblock.innerHTML = "Your input is <b>" + args.object.html + "</b>.";
+  author: {
+    name: "Your Name",
+    email: "you@mozilla.com",
+    homepage: "http://labs.mozilla.com/",
   },
+  license: "GPL",
+  homepage: "http://ubiquity.mozilla.com/",
+  icon: "http://www.mozilla.com/favicon.ico",
+  arguments: [{role: "object", nountype: noun_arb_text, label: "text"}],
   execute: function execute(args) {
-    displayMessage("You selected: " + args.object.text, this);
-  }
+    displayMessage("You input: " + args.object.text, this);
+  },
+  preview: function preview(pblock, args) {
+    pblock.innerHTML = "Your input is " + args.object.html.bold() + ".";
+  },
 });
 
