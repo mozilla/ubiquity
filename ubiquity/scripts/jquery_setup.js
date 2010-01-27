76ece43d585d75405fcab565c767e441dd81d258
satyr <murky.satyr@gmail.com>
2010-01-27 19:39:27 +0900

    jquery_setup: Made jQuery.error() and jQuery.parseJSON() throw actual errors rather than plain strings.

--- ubiquity/scripts/jquery_setup.js
+++ ubiquity/scripts/jquery_setup.js
@@ -1,17 +1,24 @@
 // This sets up jQuery when it's loaded in a hidden chrome window
 // that doesn't provide a user interface (e.g., a WebJsModule).
 
-jQuery.ajaxSetup(
-  {xhr: function() {
-     // This is a fix for #470. We're going to create the XHR object
-     // from whatever current window the user's using, so that any UI
-     // that needs to be brought up as a result of the XHR is shown
-     // to the user, rather than being invisible and locking up the
-     // application.
+Components.utils.import("resource://ubiquity/modules/utils.js");
 
-     var jsm = {};
-     Components.utils.import("resource://ubiquity/modules/utils.js", jsm);
-     var window = jsm.Utils.currentChromeWindow;
-     return new window.XMLHttpRequest();
-   }
-  });
+jQuery.ajaxSetup({
+  xhr: function currentChromeXHR() {
+    // This is a fix for #470. We're going to create the XHR object
+    // from whatever current window the user's using, so that any UI
+    // that needs to be brought up as a result of the XHR is shown
+    // to the user, rather than being invisible and locking up the
+    // application.
+    return Utils.currentChromeWindow.XMLHttpRequest();
+  }
+});
+
+jQuery.error = function nativeError(msg) {
+  throw Components.Exception(
+    msg,
+    Components.results.NS_ERROR_FAILURE,
+    Components.stack.caller);
+};
+
+jQuery.parseJSON = JSON.parse;
